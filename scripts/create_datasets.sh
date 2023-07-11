99e99451f8924cc650a67ebc3fbba104b2008e6c
Matheus Pereira <matpereira@microsoft.com>
2023-07-11 06:49:09 -0700

    use /tmp for the datasets preparation

--- scripts/create_datasets.sh
+++ scripts/create_datasets.sh
@@ -2,33 +2,31 @@
 
 set -e
 
-mkdir -p tmp
-
 ### Natural Instructions ###
 
-git clone https://github.com/allenai/natural-instructions.git tmp/ni
+git clone https://github.com/allenai/natural-instructions.git /tmp/ni
 
 for num_examples in 16 32 64 100 1000; do
 for seed in 13 42 58; do
     echo "Working on seed: $seed"
-    python misc/create_ni_dataset.py tmp/ni --output_path ./dataloader/ni_data/processed/ --seed ${seed} --num_examples ${num_examples}
+    python misc/create_ni_dataset.py /tmp/ni --output_path ./dataloader/ni_data/processed/ --seed ${seed} --num_examples ${num_examples}
 done
 done
 
-rm -rf tmp/ni
+rm -rf /tmp/ni
 
 ### CrossFit ###
 
-git clone https://github.com/INK-USC/CrossFit.git tmp/xfit
+git clone https://github.com/INK-USC/CrossFit.git /tmp/xfit
 
-cd tmp/xfit/tasks/
+cd /tmp/xfit/tasks/
 python _build_gym.py --build --n_proc 20
 cd ..
 mkdir -p ../../dataloader/xfit_data/processed
 mv data ../../dataloader/xfit_data/processed
 cd ../..
 
-rm -rf tmp/xfit
+rm -rf /tmp/xfit
 
 ## T0 ##
 
