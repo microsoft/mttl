1fb3aa5673340e9889722e2df2084223041e5894
oleksost <ostaoy2@gmail.com>
2023-08-15 22:23:48 +0000

    log cluster routing statistics

--- inst_follow/models/clm.py
+++ inst_follow/models/clm.py
@@ -3,6 +3,9 @@ import os
 import sys
 import torch     
 import copy
+import numpy as np    
+import pytorch_lightning as pl
+import matplotlib.pyplot as plt
 from torch.optim.optimizer import Optimizer     
 import wandb          
 from collections import defaultdict
@@ -16,6 +19,8 @@ from mttl.models.modify_model import modify_transformer
 from mttl.models.utils import EfficientCheckpointModule, RoutingInfo, get_global_batch_size
 from mttl.utils import freeze_embeds
 from pytorch_lightning.utilities.parsing import AttributeDict
+ 
+EPS = 1e-12
 
 class CLM(EfficientCheckpointModule):
     def __init__(self, **kwargs):
@@ -28,7 +33,7 @@ class CLM(EfficientCheckpointModule):
         self.tokenizer = kwargs["tokenizer"]
         self.pad_token_id = self.tokenizer.pad_token_id
         self.model: AutoModelForCausalLM = None
-        self.accumulate_metrics = defaultdict(list)
+        self.accumulate_metrics_batch = defaultdict(list)
         if kwargs.get('model_object') is None:
             raise NotImplementedError()
             self.model = AutoModelForSeq2SeqLM.from_pretrained(
@@ -73,7 +78,8 @@ class CLM(EfficientCheckpointModule):
             self.loss_plugins[plugin.name] = plugin
         else:
             self.loss_plugins = nn.ModuleDict({plugin.name: plugin})
-
+    
+           
     def forward(self, batch, reduction='mean'):  
         input_ids, labels = batch["input_ids"], batch["labels"]       
         self.model.task_id_container["routing_infos"] = RoutingInfo.from_batch(batch) # pad tokens also have -100
@@ -110,14 +116,16 @@ class CLM(EfficientCheckpointModule):
         aux_loss = torch.mean(torch.stack(self.model.task_id_container["routing_infos"].aux_loss)) if len(self.model.task_id_container["routing_infos"].aux_loss)>0 else 0
         
         
-        # log metrics if training
+        # we accumulate metrics over the microbatches
         for k,v in self.model.task_id_container["routing_infos"].metrics.items():        
-                self.accumulate_metrics[k].append(torch.tensor(v).mean())
+                self.accumulate_metrics_batch[k].append(torch.tensor(v))
         
         
         return loss, aux_loss
     
-    def calculate_routing_mask(self, x, routing_infos):  
+    def calculate_routing_mask(self, x, routing_infos):
+        if not hasattr(self.args, "x_routing_option"):
+            return None  
         # self.args.x_routing_option=4  
         padding_mask = None
         bs, seq = x.shape
@@ -128,7 +136,7 @@ class CLM(EfficientCheckpointModule):
             padding_mask = routing_infos.pad_token_mask # 1 if the token is not a pad token, so its either instruciton or output
             instruction_mask = torch.ones_like(padding_mask) # 1 if the token is part of instruction or pad token (so outputs are 0s)
             
-            if routing_infos.labels is not None:
+            if routing_infos.labels is not None:          
                 instruction_mask = (routing_infos.labels==-100).float() # 1 if the token is part of instruction or pad token (so outputs are 0s)
             if self.args.x_routing_option==1 or (self.args.x_routing_option==2 and gen_mode): # here we only use instruction to decide about the routing
                 padding_mask = padding_mask * instruction_mask # 1 if the token is part of instruction
@@ -185,9 +193,13 @@ class CLM(EfficientCheckpointModule):
                 raise NotImplementedError()
          
         return padding_mask
-        
-    
-
+           
+    def compute_routings(self, batch, **kwargs):      
+        out = self.generate(batch, save_oracle_routings=True, gen_mode=0, **kwargs)
+        oracle_routings = self.model.task_id_container["routing_infos"].oracle_routings
+        return out, oracle_routings
+       
+     
     def generate(self, batch, **kwargs):        
         if not hasattr(self.model, "task_id_container"):
             self.model.task_id_container = {}                          
@@ -195,19 +207,28 @@ class CLM(EfficientCheckpointModule):
         setattr(self.model.task_id_container["routing_infos"], "gen_mode", 1)
         padding_mask = self.calculate_routing_mask(batch["input_ids"], self.model.task_id_container["routing_infos"])
         setattr(self.model.task_id_container["routing_infos"], "pad_token_mask", padding_mask)
+        
+        # if routings are given (should be oracle routings), we will use them for generation
+        if "routings" in kwargs:           
+            setattr(self.model.task_id_container["routing_infos"], "routings", kwargs["routings"])
+            kwargs.pop("routings")
+                 
+        # if flag is set, we will store the oracle routings
+        if "save_oracle_routings" in kwargs:  
+            setattr(self.model.task_id_container["routing_infos"], "save_oracle_routings", kwargs["save_oracle_routings"])
+            kwargs.pop("save_oracle_routings")
+        
+        if "gen_mode" in kwargs:  # so that in xr4 we look at both nput and output   
+            setattr(self.model.task_id_container["routing_infos"], "gen_mode", kwargs["gen_mode"])
+            kwargs.pop("gen_mode")
+         
         return self.model.generate(
             inputs=batch["input_ids"],
             **kwargs
         )
     
     def on_before_optimizer_step(self, optimizer: Optimizer, optimizer_idx: int) -> None:
-        # if wandb.run is not None:                
-            # pl logger logs every 50 steps, we want to log before every update
-        for k,v in self.accumulate_metrics.items():
-                # log to wandb directly                   
-                # wandb.log({f"train/{k}": torch.tensor(v).mean()})
-                self.log(f"train/{k}", torch.tensor(v).mean(), on_step=True)
-        self.accumulate_metrics = defaultdict(list)
+        self.log_routing_metrics()
         return super().on_before_optimizer_step(optimizer, optimizer_idx)
     
     def training_step(self, batch, _):   
@@ -231,21 +252,67 @@ class CLM(EfficientCheckpointModule):
             self.log(f"train/lr_{i}", pg["lr"])
         return total_loss
 
+    def log_routing_metrics(self, stage="train"):
+        
+        # we need to keep online mean ove rthe metrics over mictobatches (s.t. the metrics are calculated for the whole batch and not microbatches) 
+        divs, entropies = [], []
+        for k,v in self.accumulate_metrics_batch.items():
+                # log to wandb directly                   
+                # wandb.log({f"train/{k}": torch.tensor(v).mean()})
+            if not "model.layers." in k: # we dont want to log the metrics for each layer
+                # self.log(f"train/{k}", torch.tensor(v).mean(), on_step=True)
+                pass
+            else:               
+                # log per lyer metrics: -MI and entropy, calculated over minimatch
+                layer_routing_dist = torch.cat(v, dim=0)
+                layer_routing_dist = layer_routing_dist.view(-1, layer_routing_dist.shape[-2], layer_routing_dist.shape[-1])
+                bs = layer_routing_dist.shape[0]
+                n_skills, n_splits = self.args.n_skills, self.args.n_splits
+                # calculate entropy and diversity over the full batch        
+                mixing_weights_ = layer_routing_dist.view(-1, n_skills)#.detach() # ex x n_skills        
+                mixing_weights_mean = layer_routing_dist.transpose(0,1).mean(dim=1) # n_splits x n_skills
+                
+                average_normalized_entropy = -torch.sum(mixing_weights_ * torch.log(mixing_weights_ + EPS), dim=-1)/ np.log(n_skills) if n_skills>1 else torch.ones_like(mixing_weights_[:,0]) # ex 
+                # solit in n_splits chunks     
+                average_normalized_entropy = average_normalized_entropy.reshape(bs, n_splits).mean(dim=0) # bs    
+                # how different are the routinf for different examples? calculate MI, entropy of average - average entropy
+                mixing_weights_mean = layer_routing_dist.transpose(0,1).mean(dim=1) # n_splits x n_skills        
+                entropy_of_av_normalized = -torch.sum(mixing_weights_mean * torch.log(mixing_weights_mean + EPS), dim=-1)/ np.log(n_skills) if n_skills>1 else torch.zeros_like(mixing_weights_mean[0]) # ex
+                div = (entropy_of_av_normalized - average_normalized_entropy).mean() # mean over n_splits
+                entropy = average_normalized_entropy.mean().item()      
+                # if not len(self.loggers)>0 or not isinstance(self.loggers[0], pl.loggers.wandb.WandbLogger):
+                self.log(f"{stage}/{k}_div", div)#, on_step=True)    
+                self.log(f"{stage}/{k}_entropy", entropy)#, on_step=True)                        
+                divs.append(div)
+                entropies.append(entropy)
+        # log mean over all layers divs and entropies
+        if len(divs)>0:
+            self.log(f"{stage}/div_layers_mean", torch.tensor(divs).mean())#, on_step=True)     
+            self.log(f"{stage}/entropy_layers_mean", torch.tensor(entropies).mean())#, on_step=True)  
+            # log distribution
+            # if len(self.loggers)>0 and isinstance(self.loggers[0], pl.loggers.wandb.WandbLogger) and stage=="val":
+            #     _ = plt.bar(range(len(divs)), divs)
+            #     self.loggers[0].log_image(f"{stage}/div_layers_dist", [wandb.Image(plt)], step=self.global_step, commit=False)                
+            #     _ = plt.bar(range(len(entropies)), entropies)
+            #     self.loggers[0].log_image(f"{stage}/entropy_layers_dist", [wandb.Image(plt)], step=self.global_step, commit=False)
+                                               
+        self.accumulate_metrics_batch = defaultdict(list)       
+                
+
     def validation_step(self, batch, batch_idx, log=True): 
-        self.accumulate_metrics = defaultdict(list)
+        self.accumulate_metrics_batch = defaultdict(list)  
         loss, aux_loss = self.forward(batch, reduction='none')
         total_loss = loss #+aux_loss 
         # outputs = self.model.forward(**batch, reduction='none')
         # loss= outputs.loss      
         mean_loss = total_loss.sum() / loss.size(0)
         if log:
-            self.log("val/loss", mean_loss, on_epoch=True, prog_bar=True)  
-            self.log("val/aux_loss", aux_loss, on_epoch=True, prog_bar=True)
-            for k,v in self.model.task_id_container["routing_infos"].metrics.items(): 
-                self.log(f"val/{k}", torch.tensor(v).mean(), on_epoch=True, prog_bar=True)
-            # self.log("val/total_loss", total_loss, on_epoch=True, prog_bar=True)
+            self.log("val/loss", mean_loss, on_epoch=True, prog_bar=True) 
+            self.log("val/aux_loss", aux_loss, on_epoch=True, prog_bar=True)                 
+            if batch_idx%(self.args.gradient_accumulation_steps*self.args.micro_batch_size)==0 and batch_idx>0: #to accumulate over larger batch
+                self.log_routing_metrics(stage="val")
         return loss, batch['task_ids']
-
+    
     def on_before_backward(self, loss: Tensor) -> None:
         return super().on_before_backward(loss)
     
@@ -271,6 +338,7 @@ class CLM(EfficientCheckpointModule):
                 on_epoch=True,
                 prog_bar=True,
             )
+        self.accumulate_metrics_batch = defaultdict(list)
         return losses
     
     def validation_epoch_end(self, outputs):     
@@ -283,6 +351,8 @@ class CLM(EfficientCheckpointModule):
             for task_id in torch.unique(task_ids):
                 task_losses[task_id.item()] = losses[task_ids == task_id].mean().item()
             f.write(json.dumps(task_losses) + "\n")
+        self.accumulate_metrics_batch = defaultdict(list)
+        self.log_routing_metrics(stage="val")
 
     def configure_optimizers(self):       
         args = self.args                     
