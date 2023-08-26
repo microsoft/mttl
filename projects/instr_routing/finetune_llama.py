b562e3cc7bbb322b0829e5f09c2684aced074d6b
Alessandro Sordoni <alessandro.sordoni@gmail.com>
2023-08-26 21:36:55 +0000

    move routing config outside

--- projects/instr_routing/finetune_llama.py
+++ projects/instr_routing/finetune_llama.py
@@ -19,6 +19,7 @@ from mttl.models.monitors import get_monitors
 from mttl.utils import get_mlf_logger
 from mttl.models.modify_model import modify_transformer
 from transformers import AutoModelForCausalLM, LlamaForCausalLM
+from config import RoutingConfig
 
 from huggingface_hub import login
 
@@ -36,84 +37,6 @@ def remove_non_serializable(d):
             remove_non_serializable(v)
         elif not json.dumps(v, default=lambda x: None):
             del d[k]
-
-
-class RoutingConfig(Config):
-    def _set_defaults(self):
-        super()._set_defaults()
-
-        self.rank = 1
-        self.load_dtype = "float32"
-        self.prune_unused_loras = True
-        self.init_b_random = False
-        self.lora_dropout = 0
-        self.lora_alpha = 16
-        self.same_lora_init = 0
-        self.load_in_8bit = False
-        self.micro_batch_size = 4
-        self.share_lora_at_attn = 0
-        self.share_lora_a = False
-        self.merge_A_B_seperately = True
-        self.train_on_inputs = False
-        self.padding_side = "right"
-        self.adapter_modules = None
-        self.router_selector_use_distances = False
-        self.adapter_layers = 0  # llama adapter
-        self.adapter_len = 0  # llama adapter
-        self.use_4_bit_backbone = False
-        self.wandb_project = None
-        self.switch_to_average = 0
-
-        self.router_weight_decay = None
-        self.param_names_added_to_sd = ""  # define additional params that will be added to state dict additionally to the trainable ones.
-        self.predict_cluster = None  # topic or skill
-        self.dst_dir = None  # dir of jsonl dataset
-
-        self.fast_dev_run = False
-        self.fast_debug_run = False
-
-        self.hf_token_hub = None
-        self.eval_ds_limit = 1
-        self.train_only_cluster = None
-        self.validation_portion = 0.03
-        self.per_cluster_test = False
-        self.use_test_set = False  # wether to use examples marked as is_test = 1 in ClusterInfo as test set
-        self.aux_mi_loss_factor = 1
-
-        # XRouter
-        self.xrouter_load_balancing = False
-        self.xrouter_x_cond = True
-        self.xrouting_option = 0  # only applies to x_router routing, depreciated
-        self.xrouter_normalize_weights = False
-        self.xrouter_normalize_input = False
-        self.xrouter_reverse_kl = False
-        self.xrouter_normal_innit = True
-        self.xrouter_use_attn = False
-        self.xrouter_sim_metric = "kl"
-        self.xrouting_sep_teacher_student = False
-        self.xrouter_init_scale = 0.02
-        self.xrouter_x4target_detach = True
-        self.xr4_option = None  # "switch" #, "default", "teacher_output"
-        self.xrouter_x4_target = "posterior"  # , "prior" -- wich router logits to use for x4 at trainign time.
-
-        self.router_learning_rate = None
-        self.eval_hellaswag = True
-        self.eval_arc = True
-        self.eval_truthfulqa = True
-        self.eval_superni = True
-        self.eval_mmlu = True
-        self.eval_batches = 50
-        self.gen_alpaca_eval = False
-
-        self.data_dir = os.getenv("AMLT_DATA_DIR", "~/data/")
-        self.output_dir = os.getenv("AMLT_OUTPUT_DIR", "tmp/instruction_learning/")
-
-    def post_init(self):
-        # to reproduce setup in https://github.com/daanelson/alpaca-lora
-        self.gradient_accumulation_steps = (
-            self.train_batch_size // self.micro_batch_size
-        )
-        self.train_batch_size = self.micro_batch_size
 
 
 def run_multitask(args):
@@ -165,13 +88,14 @@ def run_multitask(args):
     if mlf_logger:
         loggers.append(mlf_logger)
 
-    loggers.append(pl.loggers.TensorBoardLogger(save_dir=args.output_dir))
+    tb_logger = pl.loggers.TensorBoardLogger(save_dir=args.output_dir)
+    loggers.append(tb_logger)
     loggers.append(pl.loggers.CSVLogger(save_dir=args.output_dir, name="csv_metrics"))
 
     kwargs = {"val_check_interval": args.eval_every} if args.eval_every else {}
 
     # get metric monitors for models
-    callbacks = get_monitors(args)
+    callbacks = []
     callbacks.append(ProgressCallback())
 
     monitor = "val/loss"
@@ -269,7 +193,7 @@ def run_multitask(args):
         )
         if wandb.run is not None:
             wandb.log({"rouge_L_super_ni": rouge_L_super_ni})
-
+        tb_logger.experiment.add_scalar("tasks/sni", rouge_L_super_ni, trainer.global_step)
         print("SuperNI RougeL: {:.2f}".format(rouge_L_super_ni))
 
     if args.eval_mmlu:
@@ -285,6 +209,7 @@ def run_multitask(args):
         )
         if wandb.run is not None:
             wandb.log({"mmlu_acc": acc})
+        tb_logger.experiment.add_scalar("tasks/mmlu", acc, trainer.global_step)
         print("MMLU accuracy: {:.2f}".format(acc))
 
 
