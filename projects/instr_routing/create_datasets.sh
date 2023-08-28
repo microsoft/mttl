f546588a378e571ba53465707e7088885489a626
Alessandro Sordoni <alessandro.sordoni@gmail.com>
2023-08-28 02:44:50 +0000

    datasets script

---
+++ projects/instr_routing/create_datasets.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+
+mkdir -p data/
+cd data
+
+### Natural Instructions ###
+git clone https://github.com/allenai/natural-instructions.git sni
+export NI_DATA_DIR=$PWD/sni
+rm -rf /tmp/ni
+
+### MMLU ###
+mkdir -p mmlu
+cd mmlu
+
+wget https://people.eecs.berkeley.edu/~hendrycks/data.tar
+tar -xvf data.tar
+rm data.tar
+export MMLU_DATA_DIR=$PWD/data
+
+cd ../..
