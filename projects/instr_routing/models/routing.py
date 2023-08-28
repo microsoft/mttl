43d1aa6d043a8bad40d3e378174b245549b27c6a
Alessandro Sordoni <alessandro.sordoni@gmail.com>
2023-08-28 02:49:50 +0000

    adapter_type default

--- projects/instr_routing/models/routing.py
+++ projects/instr_routing/models/routing.py
@@ -176,8 +176,9 @@ class AuxRoutingLoRALinear(RoutingAdapter):
 @register_modifier("vsmear")
 def modify_with_vsmear(transformer, config):
     config.router_selector = config.router_selector or "vsmear"
+    config.adapter_type = config.adapter_type or "lora"
 
-    if config.adapter_type in ["lora", None]:
+    if config.adapter_type in ["lora"]:
         return modify_with_routing(transformer, config, AuxRoutingLoRALinear, RouterWrapper)
     else:
         raise NotImplementedError(f"Adapter type {config.adapter_type} not implemented for vsmear modifier.")
