6c890c6e1147219fe61b7a69c7ccc43746dacce7
Alessandro Sordoni <alessandro.sordoni@gmail.com>
2023-08-28 15:59:16 +0000

    add router to params!!!

--- projects/instr_routing/run_vsmear.sh
+++ projects/instr_routing/run_vsmear.sh
@@ -1,9 +1,9 @@
 /bin/rm -rf /tmp/runs/vsmear/
 python finetune_llama.py \
-    -c configs/platypus/gptneo_125m.json+configs/platypus/gptneo_125m_dense.json \
+    -c configs/platypus/gptneo_125m.json+configs/platypus/gptneo_125m_vsmear.json \
     -k output_dir=/tmp/runs/vsmear/ \
     eval_superni=True \
     eval_mmlu=True \
     n_skills=8 \
-    model_modifier=vsmear \
-    total_steps=1000
+    total_steps=1000 \
+    $*
