17fd63ca63171095458aa1e24114233e556b1679
oleksost <ostaoy2@gmail.com>
2023-08-30 19:10:09 +0000

    wip

--- projects/instr_routing/models/vsmear.py
+++ projects/instr_routing/models/vsmear.py
@@ -150,3 +150,86 @@ def modify_with_vsmear(transformer, config):
     config.router_selector = "vsmear"
 
     return modify_with_smear(transformer, config)
+
+
+
+@register_selector("vsmear_w_reg")          
+class VSMEARRouterExperimental(VSMEARRouter):
+    def __init__(self, config, in_d):
+        super().__init__(config, in_d)
+
+    def forward(self, routing_infos, input: torch.Tensor):
+        padding_mask = routing_infos.pad_token_mask
+        inst_padding_mask = routing_infos.inst_token_mask
+
+        prior_input = self.apply_mask_and_average(input, inst_padding_mask)
+        prior_routes = self.route(self.prior_router, self.prior_router_ln, prior_input)
+
+        if self.training:
+            # during training :-)
+            post_input = self.apply_mask_and_average(input, padding_mask)
+            post_routes = self.route(self.post_router, self.post_router_ln, post_input)
+            routing_probs = F.softmax(post_routes, dim=-1)
+
+            # compute auxiliary loss (KL divergence), KL = - H(posterior) + Xent(posterior, prior)
+            auxiliary_loss = routing_probs * F.log_softmax(
+                post_routes, -1
+            ) - routing_probs * F.log_softmax(prior_routes, dim=-1)
+            auxiliary_loss = auxiliary_loss.sum(dim=-1).mean()
+        else:
+            # during eval :-(
+            routing_probs = F.softmax(prior_routes, dim=-1)
+            auxiliary_loss = routing_probs.sum().detach() * 0.0
+        return routing_probs.unsqueeze(1), auxiliary_loss
+
+
+class AuxRoutingLoRALinear_wreg(SkilledLoRA, RoutingMixin):
+    def __init__(self, config, task_id_ptr, layer, selector=None, **kwargs):
+        RoutingMixin.__init__(self, task_id_ptr)
+        SkilledLoRA.__init__(self, config, layer, **kwargs)
+
+        if selector is None:
+            self.selector = get_selector(config, in_d=self.in_features)
+        else:
+            self.selector = selector
+
+        # store losses and metrics
+        self.losses = []
+        self.metrics = {}
+
+    def forward(self, input):
+        task_id = self.routing_infos.task_ids
+        repeat = input.size(0) // task_id.size(0)
+
+        # this repeat follows the patten in `model.predict()` line 152
+        if repeat:
+            self.routing_infos.repeat_interleave(repeat)
+
+        if self.selector is not None:
+            mixing_weights = self.selector(self.routing_infos, input=input)
+            if isinstance(mixing_weights, tuple):
+                mixing_weights, kl = mixing_weights
+                self.losses.append(kl)
+        else:
+            bs = input.size(0)
+            mixing_weights = torch.ones(
+                bs, self.n_splits, self.n_skills, device=input.device, dtype=input.dtype
+            )
+
+        self.metrics["routing"] = mixing_weights.detach().cpu().float()
+        return SkilledLoRA.forward(self, input, mixing_weights)
+
+  
+@register_modifier("vsmear_w_reg")            
+def modify_with_vsmear_reg(transformer, config):
+    config.router_selector = "vsmear_w_reg"
+    config.adapter_type = "lora"
+
+    if config.adapter_type in ["lora"]:
+        return modify_with_routing(     
+            transformer, config, AuxRoutingLoRALinear_wreg, RouterWrapper
+        )
+    else:
+        raise NotImplementedError(
+            f"Adapter type {config.adapter_type} not implemented for vsmear modifier."
+        )
