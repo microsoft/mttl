078935e747c323b250cefaba8dca26518cc56d4d
Alessandro Sordoni <alessandro.sordoni@gmail.com>
2023-08-30 01:48:31 +0000

    use else in get tokenizer

--- mttl/datamodule/utils.py
+++ mttl/datamodule/utils.py
@@ -8,17 +8,16 @@ def get_tokenizer(config):
         tokenizer = LlamaTokenizerFast.from_pretrained(config.model)
         tokenizer.pad_token_id = 0 
         tokenizer.padding_side = 'left'
-        return tokenizer
-    
-    tokenizer = AutoTokenizer.from_pretrained(config.model, use_fast=True)
-    tokenizer.model_max_length = int(1e9)
+    else:
+        tokenizer = AutoTokenizer.from_pretrained(config.model)
+        tokenizer.model_max_length = int(1e9)
 
-    if config.model_family == 'gpt':
-        tokenizer.padding_side = 'left'
+        if config.model_family == 'gpt':
+            tokenizer.padding_side = 'left'
 
-    if tokenizer.pad_token_id is None:
-        logger.warn("Setting pad_token_id to 0, given that pad_token_id was not detected.")
-        tokenizer.pad_token_id = 0
+        if tokenizer.pad_token_id is None:
+            logger.warn("Setting pad_token_id to 0, given that pad_token_id was not detected.")
+            tokenizer.pad_token_id = 0
 
-    tokenizer.add_eos_token = False
+        tokenizer.add_eos_token = False
     return tokenizer
