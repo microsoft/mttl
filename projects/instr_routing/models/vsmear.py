338b554302fb5c0af40f76f18c9617b1041cbedd
Alessandro Sordoni <alessandro.sordoni@gmail.com>
2023-09-01 16:35:12 +0000

    smear kl term

--- projects/instr_routing/models/vsmear.py
+++ projects/instr_routing/models/vsmear.py
@@ -131,7 +131,7 @@ class VSMEARRouter(SMEARRouter):
             self.metrics["h_post"] = h_post / math.log(self.n_skills)
             self.metrics["h_pri"] = h_pri / math.log(self.n_skills)
             self.metrics["x_ent"] = x_ent
-            self.auxiliary_loss = x_ent
+            self.auxiliary_loss = -h_post + x_ent
         else:
             # during eval :-(
             prior_probs = routing_probs = F.softmax(prior_routes, dim=-1)
