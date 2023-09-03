046b49725193436328d80b1acdce5ab958af81af
Alessandro Sordoni <alessandro.sordoni@gmail.com>
2023-09-03 03:34:01 +0000

    fix bug in label_smoothed_nll_loss when ignore_index is negative

--- mttl/utils.py
+++ mttl/utils.py
@@ -35,16 +35,14 @@ def label_smoothed_nll_loss(
     """From fairseq"""
     if target.dim() == lprobs.dim() - 1:
         target = target.unsqueeze(-1)
+
+    pad_mask = target.eq(ignore_index)
+    # because otherwise we gather -100 :-(
+    target.masked_fill_(pad_mask, 0)
     nll_loss = -lprobs.gather(dim=-1, index=target)
     smooth_loss = -lprobs.sum(dim=-1, keepdim=True)
-
-    if ignore_index is not None:
-        pad_mask = target.eq(ignore_index)
-        nll_loss.masked_fill_(pad_mask, 0.0)
-        smooth_loss.masked_fill_(pad_mask, 0.0)
-    else:
-        nll_loss = nll_loss.squeeze(-1)
-        smooth_loss = smooth_loss.squeeze(-1)
+    nll_loss.masked_fill_(pad_mask, 0.0)
+    smooth_loss.masked_fill_(pad_mask, 0.0)
 
     if reduction == "mean":
         nll_loss = nll_loss.sum()
