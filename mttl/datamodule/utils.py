e12f04844f3d1f444c5bd0a5ea0f9fd7d945daca
Alessandro Sordoni <alessandro.sordoni@gmail.com>
2023-09-07 19:28:52 +0000

    padding side as an option

--- mttl/datamodule/utils.py
+++ mttl/datamodule/utils.py
@@ -15,8 +15,19 @@ def get_tokenizer(config, for_generation=False):
 
     if config.model_family == 'gpt':
         if for_generation:
+            if config.padding_side == 'right':
+                logger.warn("padding side is 'right', but we are in generation mode!")
+
             logger.warn("for_generation is True, setting padding_side for tokenizer to 'left'.")
+            
             tokenizer.padding_side = 'left'
+        else:
+            if hasattr(config, 'padding_side'):
+                logger.warn("Setting padding side to {}".format(config.padding_side))
+
+                tokenizer.padding_side = config.padding_side
+            else:
+                logger.warn("Padding side is {}".format(tokenizer.padding_side))
 
         # do not add eos token, we will add it accordingly *if* needed.
         tokenizer.add_eos_token = False
