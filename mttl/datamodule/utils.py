1510f5307bf206489f031f8e48176b25db626032
Alessandro Sordoni <alsordon@microsoft.com>
2023-09-11 15:12:08 -0700

    get_tokenizer_with_args

--- mttl/datamodule/utils.py
+++ mttl/datamodule/utils.py
@@ -4,36 +4,45 @@ from mttl.utils import logger
 
 
 def get_tokenizer(config, for_generation=False):
-    if "llama" in config.model:
-        tokenizer = LlamaTokenizerFast.from_pretrained(config.model)
+    return get_tokenizer_with_args(
+        config.model, config.model_family, config.padding_side, for_generation
+    )
+
+
+def get_tokenizer_with_args(
+    model_name, model_family, padding_side="right", for_generation=False
+):
+    if "llama" in model_name:
+        tokenizer = LlamaTokenizerFast.from_pretrained(model_name)
         tokenizer.model_max_length = int(1e9)
-        if not config.model_family == "gpt":
-            raise ValueError("We detected a Llama model, but model_family != 'gpt', fix your config!")
+        if not model_family == "gpt":
+            raise ValueError(
+                "We detected a Llama model, but model_family != 'gpt', fix your config!"
+            )
     else:
-        tokenizer = AutoTokenizer.from_pretrained(config.model)
+        tokenizer = AutoTokenizer.from_pretrained(model_name)
         tokenizer.model_max_length = int(1e9)
 
-    if hasattr(config, 'padding_side'):
-        logger.warn("Setting padding side to {}".format(config.padding_side))
+    logger.warn("Setting padding side to {}".format(padding_side))
+    tokenizer.padding_side = padding_side
 
-        tokenizer.padding_side = config.padding_side
-    else:
-        logger.warn("Padding side is {}".format(tokenizer.padding_side))
-
-    if config.model_family == 'gpt':
+    if model_family == "gpt":
         if for_generation:
-            if config.padding_side == 'right':
+            if padding_side == "right":
                 logger.warn("Padding side is 'right', but we are in generation mode!")
 
-            logger.warn("for_generation is True, setting padding_side for tokenizer to 'left'.")
-            
-            tokenizer.padding_side = 'left'
+            logger.warn(
+                "for_generation is True, setting padding_side for tokenizer to 'left'."
+            )
+            tokenizer.padding_side = "left"
 
         # do not add eos token, we will add it accordingly *if* needed.
         tokenizer.add_eos_token = False
 
     if tokenizer.pad_token_id is None:
-        logger.warn("Setting pad_token_id to 0, given that pad_token_id was not detected.")
+        logger.warn(
+            "Setting pad_token_id to 0, given that pad_token_id was not detected."
+        )
         tokenizer.pad_token_id = 0
 
     return tokenizer
