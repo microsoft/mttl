42be8fcb32b09a60ca1d7a3f993fb103799254aa
Alessandro Sordoni <alsordon@microsoft.com>
2023-09-14 19:27:12 -0700

    fix prepare for kbit training

--- projects/instr_routing/models/clm.py
+++ projects/instr_routing/models/clm.py
@@ -36,42 +36,15 @@ class AugmentedRoutingInfo(RoutingInfo):
     # layer_name -> tensor, holds the encoding for the instruction during generation
     # this is needed because the instruction is not passed as input during generation of subsequent tokens
     inputs_cache_for_generation: Dict[object, torch.Tensor] = field(default_factory=dict)
+    # aux losses from each layer during training
+    aux_losses: Dict[object, torch.Tensor] = field(default_factory=dict)
 
 
-
-def prepare_model_for_kbit_training(model, use_gradient_checkpointing=True):
-    r"""
-    This method wraps the entire protocol for preparing a model before running a training. This includes:
-        1- Cast the layernorm in fp32 2- making output embedding layer require grads 3- Add the upcasting of the lm
-        head to fp32
-
-    Args:
-        model, (`transformers.PreTrainedModel`):
-            The loaded model from `transformers`
-    """
-    loaded_in_kbit = getattr(model, "is_loaded_in_8bit", False) or getattr(
-        model, "is_loaded_in_4bit", False
-    )
-
+def prepare_model_for_kbit_training(model):
     # cast all non INT8 parameters to fp32
     for param in model.parameters():
         if (param.dtype == torch.float16) or (param.dtype == torch.bfloat16):
             param.data = param.data.to(torch.float32)
-
-    if loaded_in_kbit and use_gradient_checkpointing:
-        # For backward compatibility
-        if hasattr(model, "enable_input_require_grads"):
-            model.enable_input_require_grads()
-        else:
-
-            def make_inputs_require_grad(module, input, output):
-                output.requires_grad_(True)
-
-            model.get_input_embeddings().register_forward_hook(make_inputs_require_grad)
-
-        # enable gradient checkpointing for memory efficiency
-        model.gradient_checkpointing_enable()
-
     return model
 
 
