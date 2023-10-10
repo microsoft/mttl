c68d005b88e5b0701083fcaa05612d6172db63e3
oleksost <ostaoy2@gmail.com>
2023-10-10 19:41:02 +0000

    removed scripts

--- projects/wiki_experts/scripts/generate_data_oai.sh
+++
@@ -1,4 +0,0 @@
-
-# conda activate comp_3.9_pl2
-#open AI
-python /home/v-oostapenko/dev/mttl/projects/wiki_experts/cli_qa_creator.py geni --model-path gpt-35-turbo --output-filename /home/v-oostapenko/mttl_out/generated_gpt35_turbo_icl5.jsonl --subset-per-subject 0.3
