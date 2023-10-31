769043346c3589b0e172891e9f3c42de375097fa
oleksost <ostapy2@gmail.com>
2023-10-31 19:47:24 +0000

    removed tracking expert_merge files

--- projects/wiki_experts/experts_merge/local_modules.py
+++
@@ -1,8 +0,0 @@
-import os
-
-base_dir = os.environ.get(
-    "MODULES_DIR", "/home/v-oostapenko/dev/amlt/wiki_experts_train_per_cat_2_if/"
-)
-base_dir_tempalte = (
-    lambda subject: f"{base_dir}/ll2_13b_expert_2_{subject}__qa-ostapeno_qa-openai_icl5_clen128_maxD-1_maxC8000_0_length_matched___5e-5_/{subject}/meta-llama_Llama-2-13b-hf-mmlu_test_oracle"
-)
