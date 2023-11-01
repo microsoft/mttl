a8b8aff9aef0f9516d61349323db5a0268ca6598
Lucas Page-Caccia <lucas.page-caccia@mail.mcgill.ca>
2023-11-01 08:25:16 -0700

    add projects to path?

--- tests/test_llama_adapter.py
+++ tests/test_llama_adapter.py
@@ -1,7 +1,7 @@
+import os
 import sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-import os
 import torch
 from pytorch_lightning import seed_everything
 from transformers import AutoTokenizer
--- tests/test_prompt_tuning.py
+++ tests/test_prompt_tuning.py
@@ -1,6 +1,6 @@
+import os
 import sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-import os
 
 import torch
 import torch.nn.functional as F
