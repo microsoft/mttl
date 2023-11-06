5f41c9014852a5d4cbe9736d0985572e08d35f29
Alessandro Sordoni <alsordon@microsoft.com>
2023-11-05 18:47:49 -0800

    remove empty test

--- tests/test_collators.py
+++
@@ -1,9 +0,0 @@
-import os
-import torch
-from transformers import AutoTokenizer
-from mttl.datamodule.collators import DefaultCollator
-from mttl.datamodule.utils import get_tokenizer
-
-
-def test_collate(tmp_path):
-    pass
