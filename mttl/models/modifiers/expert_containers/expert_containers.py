b4269e871cfbf016b43da98cfa6586d4306481c6
Lucas Page-Caccia <lucas.page-caccia@mail.mcgill.ca>
2023-11-20 11:13:22 -0800

    add KVExpertContainer

--- mttl/models/modifiers/expert_containers/expert_containers.py
+++ mttl/models/modifiers/expert_containers/expert_containers.py
@@ -4,6 +4,7 @@ from torch import nn
 from typing import Any, Dict
 from mttl.models.modifiers.base import MergeableAdapter
 from mttl.models.modifiers.lora import LoRA, SkilledLoRA
+from mttl.models.modifiers.kv_adapter import KVAdapter
 from mttl.models.modifiers.expert_containers.selectors import *
 from mttl.utils import logger
 
@@ -23,6 +24,17 @@ class ExpertContainer:
     @abstractmethod
     def forward(self, input, **kwargs):
         pass
+
+    def add_expert_to_selector(self, expert_name: str):
+        if expert_name in self.experts:
+            self.selector.add_expert(expert_name)
+            self.selector.default_expert_name = self.default_expert_name
+
+    def __getitem__(self, key):
+        return self.experts[key]
+
+    def __len__(self):
+        return len(self.experts)
 
 
 class LoRAExpertContainer(MergeableAdapter, ExpertContainer):
@@ -86,11 +98,6 @@ class LoRAExpertContainer(MergeableAdapter, ExpertContainer):
 
         self.add_expert_to_selector(name)
 
-    def add_expert_to_selector(self, expert_name: str):
-        if expert_name in self.experts:
-            self.selector.add_expert(expert_name)
-            self.selector.default_expert_name = self.default_expert_name
-
     def merge_experts_together(self, weights=None):
         """
         Merges experts to one expert according to weights, if weights are not given, it uses the selector to get the weights.
@@ -150,15 +157,63 @@ class LoRAExpertContainer(MergeableAdapter, ExpertContainer):
             weights.append(torch.tensor(ws))
         return SkilledLoRA.parallel_linear_forward(input, load_experts, weights)
 
-    def __getitem__(self, key):
-        return self.experts[key]
-
-    def __len__(self):
-        return len(self.experts)
-
     def forward(self, input, **kwargs):
         if len(self.experts) > 0:
             weights: list = self.selector(input)
             output = self.route(input, weights)
             return output
         return self.layer(input)
+
+
+class KVExpertContainer(ExpertContainer, nn.Module):
+    def __init__(self, config, task_id_container, layer, selector=None):
+        super().__init__()
+        self.config = config
+        self.layer = layer
+        self.selector: Selector = selector or TaskNameSelector()
+        self.selector.info_container = task_id_container
+
+        # Check if layer is an attention layer :
+        if not hasattr(self.layer, "k_proj"):
+            raise ValueError(
+                "`KVExpertContainer` should wrap an attention layer. {}".format(
+                    self.layer.__class__.__name__
+                )
+            )
+
+        self.info_container = task_id_container
+        self.default_expert_name = None
+        self.merged_expert_names = []
+        self.experts = nn.ModuleDict({})
+
+        # For KVExpertContainer, we "fuse" the routing and expert fwd pass
+
+    def forward(self, input, **kwargs):
+        if len(self.experts) > 0:
+            weights: list = self.selector(input)
+            output = self.route(input, weights)
+            return output
+        return self.layer(input, **kwargs)
+
+    def add_expert(
+        self,
+        name: str,
+        expert_config: Any,
+        expert_weights: Dict[str, torch.Tensor],
+        action="route",
+        is_default=False,
+    ) -> None:
+        if name in self.experts:
+            raise ValueError("An expert with name {} already exists.".format(name))
+
+        if action == "merge":
+            raise ValueError("Merging is not supported for `KVAdapters`.")
+
+        expert_module = KVAdapter(expert_config, self.layer)
+        expert_module.load_adapter_weights(expert_weights)
+        self.experts[name] = expert_module
+
+        if is_default:
+            self.default_expert_name = name
+
+        self.add_expert_to_selector(name)
