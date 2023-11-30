6cac61900bb0393e4afc5dbb4f7670e3004258f0
Alessandro Sordoni <alsordon@microsoft.com>
2023-11-30 13:32:14 -0800

    fix bug

--- mttl/models/modifiers/expert_containers/module_graph.py
+++ mttl/models/modifiers/expert_containers/module_graph.py
@@ -376,7 +376,6 @@ def load_expert(
     logger.info(f"Loading expert from {expert_checkpoint}...")
     expert_checkpoint = torch.load(expert_checkpoint, map_location="cpu")
 
-    breakpoint()
     if "hyper_parameters" in expert_checkpoint:
         # this is a PL checkpoint
         if "tokenizer" in expert_checkpoint["hyper_parameters"]:
@@ -388,11 +387,12 @@ def load_expert(
         if "tokenizer" in expert_checkpoint["hyper_parameters"]:
             expert_checkpoint["hyper_parameters"].pop("tokenizer")
 
-        if "tokenizer" in expert_info_data["expert_config"]:
-            expert_info_data["expert_config"].pop("tokenizer")
-
         if not expert_info_data.get("expert_config", None):
             expert_info_data["expert_config"] = expert_checkpoint["hyper_parameters"]
+        else:
+            if "tokenizer" in expert_info_data["expert_config"]:
+                expert_info_data["expert_config"].pop("tokenizer")
+
         if not expert_info_data.get("expert_name", None):
             expert_info_data["expert_name"] = expert_checkpoint["hyper_parameters"][
                 "expert_name"
