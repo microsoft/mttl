df4629c96534e7882f27f22d0c29419e247c3467
Alessandro Sordoni <alsordon@microsoft.com>
2023-12-22 10:14:33 -0800

    fix EM evaluator

--- mttl/evaluators/em_evaluator.py
+++ mttl/evaluators/em_evaluator.py
@@ -1,23 +1,17 @@
 import tqdm
-import torch
 import numpy as np
 
-from mttl.evaluators.base import Evaluator, switch_to_eval_mode
-from mttl.evaluators.ni_evaluator import compute_metrics
-from mttl.models.utils import transfer_batch_to_device, EfficientCheckpointModule
+from mttl.dataloader.ni_metrics import compute_metrics
+from mttl.evaluators.base import (
+    Evaluator,
+    GenerationMixin,
+    GenerationOutput,
+    switch_to_eval_mode,
+)
 from mttl.utils import logger
 
 
-def decode(preds, tokenizer):
-    preds[preds == -100] = tokenizer.pad_token_id
-    preds = tokenizer.batch_decode(
-        preds, skip_special_tokens=True, clean_up_tokenization_spaces=True
-    )
-    preds = [pred.strip() for pred in preds]
-    return preds
-
-
-class EMEvaluator(Evaluator):
+class EMEvaluator(Evaluator, GenerationMixin):
     def __init__(self, datamodule, use_vllm=False, generation_kwargs=None):
         super().__init__(
             datamodule=datamodule,
@@ -38,7 +32,6 @@ class EMEvaluator(Evaluator):
         subsample=-1,
         num_batches=None,
         verbose=True,
-        max_length=None,
         shuffle=False,
     ):
         dataloader = self.get_dataloader(split, subsample, shuffle=shuffle)
@@ -60,37 +53,8 @@ class EMEvaluator(Evaluator):
             labels_texts = batch["labels_texts"]
             sources_texts = batch["sources_texts"]
 
-            max_length = max_length or self.max_output_length
-
-            batch = transfer_batch_to_device(batch, self.device)
-            with torch.no_grad():
-                if isinstance(model, EfficientCheckpointModule):
-                    predictions = model.generate(
-                        batch,
-                        max_new_tokens=max_length,
-                        generation_config=model.generation_config,
-                        return_dict_in_generate=True,
-                        output_scores=True,
-                        **extra_kwargs,
-                        **self.generation_kwargs,
-                    )
-                else:
-                    predictions = model.generate(
-                        batch["input_ids"],
-                        attention_mask=batch["attention_mask"],
-                        max_new_tokens=max_length,
-                        generation_config=model.generation_config,
-                        return_dict_in_generate=True,
-                        output_scores=True,
-                        **extra_kwargs,
-                        **self.generation_kwargs,
-                    )
-
-            predictions = predictions.sequences
-            if self.datamodule.config.model_family == "gpt":
-                predictions = predictions[:, batch["input_ids"].shape[-1] :]
-
-            predictions = decode(predictions, self.tokenizer)
+            predictions: GenerationOutput = self.generate_for_batch(model, batch)
+            predictions = predictions.generated_texts
             references = [[l] for l in labels_texts]
 
             eval_metrics = compute_metrics(predictions, references, reduction="none")
