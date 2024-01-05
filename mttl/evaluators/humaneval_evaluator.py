84187ab6ff4cd9c435fdc3e3b085b993558f9d97
Alessandro Sordoni <alsordon@microsoft.com>
2024-01-05 09:01:24 -0800

    add stop tokens

--- mttl/evaluators/humaneval_evaluator.py
+++ mttl/evaluators/humaneval_evaluator.py
@@ -3,8 +3,13 @@ from mttl.evaluators.code_evaluator import CodeEvaluator
 
 
 class HumanEvalEvaluator(CodeEvaluator):
+    STOP_TOKENS = ["\n\n", "\ndef", "\nif", "\n#"]
+
     def __init__(self, config, **kwargs):
         datamodule = HumanEvalDataModule(config, for_generation=True)
+
+        generation_kwargs = generation_kwargs or {}
+        generation_kwargs.update({"stop_tokens": self.STOP_TOKENS})
 
         super().__init__(
             datamodule=datamodule,
