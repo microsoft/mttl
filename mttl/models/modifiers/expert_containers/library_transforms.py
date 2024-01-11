9c969ae295462f3991f5878bbd0308ddf0d42709
Lucas Page-Caccia <lucas.page-caccia@mail.mcgill.ca>
2024-01-10 17:58:46 -0800

    centroid compute transform

--- mttl/models/modifiers/expert_containers/library_transforms.py
+++ mttl/models/modifiers/expert_containers/library_transforms.py
@@ -1,12 +1,16 @@
 from dataclasses import dataclass
 from mttl.models.modifiers.expert_containers.expert_library import HFExpertLibrary
 from mttl.models.modifiers.expert_containers.module_graph import Expert
+from projects.wiki_experts.src.expert_model import MultiExpertModel
 from mttl.utils import logger
 from mttl.models.modifiers.modify_model import get_modifier_type
+from mttl.models.utils import model_loader_helper
 from typing import Optional
+from mttl.models.utils import EfficientCheckpointModule, transfer_batch_to_device
 
 import copy
 import torch
+import torch.nn.functional as F
 from tqdm import tqdm
 import numpy as np
 import sklearn.decomposition
@@ -140,3 +144,102 @@ class WeightedLinearMerge(LibraryTransform):
             base_expert.expert_weights[k] /= len(experts)
 
         return base_expert
+
+
+@dataclass
+class PrototypeComputerConfig:
+    use_base_model_only: bool = (
+        False  # This computes sentence embeddings without the adapter
+    )
+    model: str = None  # If `use_base_model_only`, can pass a specific model to compute embeddings with
+    max_samples_per_task: int = 100
+
+
+class DatasetCentroidComputer(LibraryTransform):
+    """
+    Encodes a dataset and computes the average embedding
+    """
+
+    def __init__(self, config: PrototypeComputerConfig = None):
+        super().__init__(config or PrototypeComputerConfig())
+
+    def _fill_missing_args(self, args, default_args):
+        # TODO: put in library utils
+        for k, v in vars(default_args).items():
+            if not hasattr(args, k):
+                setattr(args, k, v)
+
+    @torch.no_grad()
+    def transform(self, library, default_args=None) -> Expert:
+        # TODO: remove project import
+        from projects.wiki_experts.train_experts_main import get_datamodule
+
+        if type(library) == str:
+            library = HFExpertLibrary(library)
+
+        logger.info("Computing centroids for {} experts".format(len(library)))
+        output = {}
+
+        for expert_name, expert in library.items():
+            training_config = expert.training_config
+            if default_args is not None:
+                self._fill_missing_args(training_config, default_args)
+
+            if self.config.use_base_model_only and self.config.model_name is not None:
+                training_config.model = self.config.model
+
+            model = MultiExpertModel(**vars(training_config)).to("cuda")
+
+            if not self.config.use_base_model_only:
+                model.add_expert_instance(expert, is_default=True)
+
+            train_tasks = expert.expert_info.expert_task_name.split(",")
+
+            centroid, count = 0, 0
+            for train_task in train_tasks:
+                # get datamodule
+                training_config.subsample_train = self.config.max_samples_per_task
+                training_config.dataset = expert.expert_info.dataset
+                training_config.finetune_task_name = train_task
+                training_config.train_batch_size = 4
+
+                dm = get_datamodule(training_config)
+                dataloader = dm.train_dataloader()
+
+                pbar = tqdm(enumerate(dataloader), total=len(dataloader))
+                device = next(model.parameters()).device
+
+                container = {}
+
+                def fetch_pre_logit_output(module, input, output):
+                    container["hidden_states"] = input[0].detach()
+
+                model.model.get_output_embeddings().register_forward_hook(
+                    fetch_pre_logit_output
+                )
+
+                for num_batch, batch in pbar:
+                    batch = transfer_batch_to_device(batch, device)
+
+                    if isinstance(model, EfficientCheckpointModule):
+                        model.forward(batch, reduction="none")
+                    else:
+                        model.forward(
+                            input_ids=batch["input_ids"],
+                            attention_mask=batch["attention_mask"],
+                        )
+
+                    bs = batch["input_ids"].size(0)
+                    hidden_states = container["hidden_states"]
+                    last_token_state = hidden_states[
+                        torch.arange(bs, device=device),
+                        batch["attention_mask"].sum(1) - 1,
+                    ]
+                    centroid += last_token_state.sum(0)
+                    count += bs
+
+            # average over all batches
+            centroid /= count
+            output[expert_name] = F.normalize(centroid, p=2, dim=-1).cpu()
+
+        return output
