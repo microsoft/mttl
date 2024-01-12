bd35c8f242f7f0300824b1d0261be19650ce93e7
Alessandro Sordoni <alsordon@microsoft.com>
2024-01-12 11:11:07 -0800

    fix tests

--- tests/test_routed_multi_expert_model.py
+++ tests/test_routed_multi_expert_model.py
@@ -257,6 +257,7 @@ class TestMultiExpertModel:
     ):
         seed_everything(0)
         config: ExpertConfig = tmp_exp_config
+        config.router_selector = "moe_rkhs_selector"
         config.moe_emb_dim = 10
         config.moe_rkhs_dim = 10
 
@@ -287,6 +288,7 @@ class TestMultiExpertModel:
     ):
         seed_everything(0)
         config: ExpertConfig = tmp_exp_config
+        config.router_selector = "moe_rkhs_selector"
         config.moe_top_k = 2
         config.moe_emb_dim = 10
         config.moe_rkhs_dim = 10
