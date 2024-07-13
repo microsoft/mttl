665bfc4c297eafb4e0271cd320769cfc05742038
zhansu <zhan.su@di.ku.dk>
2024-07-13 02:09:10 -0400

    add preference data module

---
+++ mttl/datamodule/preference_data_module.py
@@ -0,0 +1,94 @@
+from mttl.datamodule.base import DatasetConfig, DefaultCollator, DefaultDataModule
+from dataclasses import dataclass
+from mttl.models.library.expert_library import DatasetLibrary
+import torch
+
+
+@dataclass
+class DataCollatorForDPO(DefaultCollator):
+    def __call__(self, batch):
+        prompts = ["Instruct: " + item["prompt"] + "\n" for item in batch]
+        chosen_responses = ["Output: " + item["chosen"] for item in batch]
+        rejected_responses = ["Output: " + item["rejected"] for item in batch]
+
+        prompt_ids = self.tokenizer.batch_encode_plus(
+            prompts,
+            padding=True,
+            return_tensors="pt",
+            max_length=self.max_input_length,
+            truncation=True,
+        )["input_ids"]
+
+        prefered_ids = self.tokenizer.batch_encode_plus(
+            chosen_responses,
+            padding=True,
+            return_tensors="pt",
+            max_length=self.max_input_length,
+            truncation=True,
+        )["input_ids"]
+
+        disprefered_ids = self.tokenizer.batch_encode_plus(
+            rejected_responses,
+            padding=True,
+            return_tensors="pt",
+            max_length=self.max_input_length,
+            truncation=True,
+        )["input_ids"]
+
+        prompt_prefered_ids = torch.cat([prompt_ids, prefered_ids], dim=-1)
+        prompt_disprefered_ids = torch.cat([prompt_ids, disprefered_ids], dim=-1)
+
+        prompt_prefered_mask = torch.cat(
+            [torch.ones_like(prompt_ids), torch.zeros_like(prefered_ids)], dim=-1
+        )
+        prompt_disprefered_mask = torch.cat(
+            [torch.ones_like(prompt_ids), torch.zeros_like(disprefered_ids)], dim=-1
+        )
+
+        return {
+            "prompt_prefered_ids": prompt_prefered_ids,
+            "prompt_disprefered_ids": prompt_disprefered_ids,
+            "prompt_prefered_mask": prompt_prefered_mask,
+            "prompt_disprefered_mask": prompt_disprefered_mask,
+        }
+
+
+@dataclass
+class Preferencemodule(DefaultDataModule):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def setup_dataset(self):
+        train_dataset = DatasetLibrary.pull_dataset_with_retry(
+            "jondurbin/truthy-dpo-v0.1"
+        )["train"]
+
+        self.train_dataset, self.dev_dataset = self.create_train_valid_split(
+            train_dataset, 0.1
+        )
+        self.test_dataset = self.dev_dataset
+
+        self.print_infos()
+
+    @property
+    def collate_fn(self):
+        return DataCollatorForDPO(
+            tokenizer=self.tokenizer,
+            padding="longest",
+            max_input_length=self.config.max_input_length,
+            max_output_length=self.config.max_output_length,
+            return_tensors="pt",
+            model_family=self.config.model_family,
+            for_generation=self.for_generation,
+        )
+
+
+if __name__ == "__main__":
+    config = DatasetConfig(model="microsoft/phi-2")
+    datamodule = Preferencemodule(config)
+    train_dataloader = datamodule.train_dataloader()
+    val_dataloder = datamodule.val_dataloader()
+    for batch in val_dataloder:
+        print(batch)
+        breakpoint()
---
+++ projects/modular_llm/train_dpo.py
@@ -0,0 +1,215 @@
+import os
+import shutil
+import sys
+from tempfile import TemporaryDirectory
+
+import torch
+from pytorch_lightning import Trainer, seed_everything
+
+sys.path.append(os.path.join(os.path.dirname(__file__), "..", ".."))
+
+from mttl.callbacks import LiveCheckpointCallback, NanoMMLUCallback, RougeCallback
+
+# from mttl.datamodule.base import get_datamodule
+from mttl.models.expert_config import ExpertConfig
+from mttl.models.expert_model import ExpertModel, MoEModel, ExpertModelDPO
+from mttl.models.library.expert import Expert, load_expert
+from mttl.models.library.expert_library import ExpertLibrary, LocalExpertLibrary
+from mttl.models.monitors import get_monitors
+from mttl.utils import (
+    generate_random_string,
+    get_pl_loggers,
+    logger,
+    rank_zero_only_and_wait,
+    remote_login,
+    setup_logging,
+)
+from mttl.datamodule.base import DatasetConfig, DefaultCollator, DefaultDataModule
+from mttl.datamodule.preference_data_module import Preferencemodule
+from projects.modular_llm.src.transfer_matrix import TransferMatrixConfig
+from projects.modular_llm.src.transfer_matrix import run_eval as produce_transfer_matrix
+
+
+def create_transfer_matrix(args, checkpoint):
+    ########################
+    # create transfer matrix
+    config = TransferMatrixConfig()
+    for k, v in vars(args).items():
+        if k in vars(config):
+            setattr(config, k, v)
+    config.eval_base = False
+    config.eval_metric = "rougeL"
+
+    expert: Expert = load_expert(checkpoint)
+    expert.expert_info.expert_name = str(args.finetune_task_name)
+    expert.expert_info.expert_task_name = str(args.finetune_task_name)
+    temp_dir = TemporaryDirectory()
+    destination = temp_dir.name
+    LocalExpertLibrary.from_expert_dict({"checkpoint": expert}, destination=destination)
+    config.library_id = destination
+    config.finetune_task_name = (
+        args.finetune_task_name.split(",")
+        if not isinstance(args.finetune_task_name, list)
+        else args.finetune_task_name
+    )
+    if len(config.finetune_task_name) < 50:
+        produce_transfer_matrix(config, debug=False)
+    ########################
+    temp_dir.cleanup()
+
+
+def run_multitask(args: ExpertConfig):
+    seed_everything(args.seed, workers=True)
+
+    # get directory of the current file
+    setup_logging(args.output_dir)
+    logger.info("Args: {}".format(args.to_json()))
+
+    remote_login(args.remote_token)
+    expert_library = None
+    if args.library_id:
+
+        @rank_zero_only_and_wait(before=False, after=True)
+        def create_library(args):
+            expert_library = ExpertLibrary.get_expert_library(
+                repo_id=args.library_id,
+                create=True,
+                destination_id=args.destination_library_id,
+            )
+            return expert_library
+
+        expert_library = create_library(args)
+
+    loggers = get_pl_loggers(args)
+    # select dataloader
+    if args.model_modifier == "poly":
+        args.init_from_scratch = True
+        model_class = MoEModel
+    else:
+        model_class = ExpertModel
+
+    config = DatasetConfig(model=args.model)
+    dm = Preferencemodule(config)
+
+    # dm = get_datamodule(args)
+    # args.n_tasks = len(dm._task_names)
+    # args.task_names = dm._task_names
+
+    model = model_class(**vars(args), tokenizer=dm.tokenizer)
+
+    if args.rl_training == "dpo":
+        ref_model = model_class(**vars(args), tokenizer=dm.tokenizer)
+        module = ExpertModelDPO(model, ref_model)
+
+    # get metric monitors for models
+    callbacks = get_monitors(args)
+    if "mbpp" in args.dataset:
+        monitor = "downstream/mbpp"
+        mode = "max"
+    else:
+        monitor = "val/loss"
+        mode = "min"
+
+    checkpoint_callback = LiveCheckpointCallback(
+        dirpath=args.output_dir,
+        monitor=monitor,
+        save_last=True,
+        mode=mode,
+        save_each_epoch=args.save_each_epoch,
+    )
+    callbacks.append(checkpoint_callback)
+
+    val_check_interval = args.eval_every
+    if val_check_interval == -1 or val_check_interval is None:
+        val_check_interval = None
+    else:
+        val_check_interval = args.gradient_accumulation_steps * args.eval_every
+        if val_check_interval > len(dm.train_dataloader()):
+            val_check_interval = len(dm.train_dataloader())
+        elif val_check_interval > args.total_steps and args.total_steps != -1:
+            val_check_interval = args.total_steps
+
+    trainer = Trainer(
+        # devices=-1,
+        # accelerator="gpu",
+        logger=loggers,
+        num_sanity_val_steps=0,
+        default_root_dir=args.output_dir,
+        max_epochs=args.num_train_epochs,
+        max_steps=args.total_steps + 1 if args.total_steps != -1 else -1,
+        gradient_clip_val=args.max_grad_norm,
+        strategy=args.compute_strategy if args.compute_strategy else "auto",
+        callbacks=callbacks,
+        enable_checkpointing=False,
+        log_every_n_steps=args.gradient_accumulation_steps,
+        accumulate_grad_batches=args.gradient_accumulation_steps,
+        precision=(
+            int(args.precision) if args.precision in ["16", "32"] else args.precision
+        ),
+        val_check_interval=val_check_interval,
+    )
+
+    # initial validation only for a bunch of datasets... ?
+    if args.eval_before_training:
+        # validating before training fails with deepspeed
+        trainer.validate(module, dm)
+
+    if args.do_train:
+        trainer.fit(module, dm)
+
+        torch.cuda.empty_cache()
+
+        # reload best model before pushing!
+        checkpoint = (
+            checkpoint_callback.best_model_path or checkpoint_callback.last_model_path
+        )
+        if args.compute_strategy == "deepspeed":
+            from deepspeed.utils.zero_to_fp32 import (
+                convert_zero_checkpoint_to_fp32_state_dict,
+            )
+
+            new_path = checkpoint.replace(".ckpt", "_fp32.ckpt")
+
+            @rank_zero_only_and_wait(before=True, after=True)
+            def convert_ckpt(path, new_path):
+                convert_zero_checkpoint_to_fp32_state_dict(path, new_path)
+
+            convert_ckpt(checkpoint, new_path)
+            checkpoint = torch.load(new_path)
+        else:
+            checkpoint = torch.load(checkpoint)["state_dict"]
+
+        module.load_state_dict(checkpoint)
+        trainer.test(module, dm)
+
+        @rank_zero_only_and_wait(before=False, after=True)
+        def upload_library(expert_library, module):
+            if expert_library is not None:
+                # refresh expert library: so we dont overwrite the readme if the remote has changed.
+                expert_library.refresh_from_remote()
+
+                if isinstance(module, MoEModel):
+                    with expert_library.batched_commit():
+                        for expert_name in module.experts_names:
+                            expert = module.get_expert_instance(expert_name)
+                            expert_library.add_expert(expert, expert_name)
+                elif isinstance(module, ExpertModel):
+                    expert = module.as_expert()
+                    expert_name = (
+                        args.expert_name
+                        or args.finetune_task_name
+                        or generate_random_string()
+                    )
+                    expert_library.add_expert(expert, expert_name)
+                else:
+                    raise ValueError("Model class not recognized")
+
+        upload_library(expert_library, module)
+
+        if args.create_transfer_matrix:
+            create_transfer_matrix(args, checkpoint)
+
+
+if __name__ == "__main__":
+    args = ExpertConfig.parse()
+    run_multitask(args)
