7e666699f1007fbc96578fa393376a1550a1e532
Alessandro Sordoni <alsordon@microsoft.com>
2024-07-15 15:05:03 -0700

    compute hidden states for avg selector config

--- mttl/models/containers/selectors/average_activation_selector.py
+++ mttl/models/containers/selectors/average_activation_selector.py
@@ -1,11 +1,30 @@
 from dataclasses import dataclass
 from typing import Dict
+from mttl.models.library.library_transforms import (
+    HiddenStateComputer,
+    HiddenStateComputerConfig,
+)
 import torch
 from mttl.models.containers.selectors import (
     PerTokenSelector,
     PerTokenSelectorConfig,
     register_multi_expert_selector,
 )
+
+
+def get_hidden_states(library, args):
+    cfg = HiddenStateComputerConfig(
+        use_base_model_only=args.use_base_model_only,
+        max_samples_per_task=args.max_samples_per_task,
+        name=args.expert_embeds_save_name,
+        track=args.track,
+        pool=args.pool,
+    )
+    output = HiddenStateComputer(cfg).transform(
+        library, recompute=args.recompute_prototypes, default_args=args
+    )
+
+    return output
 
 
 @dataclass
