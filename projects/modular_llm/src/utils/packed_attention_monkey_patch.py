f1347270139a8bb96f078b7b32f925488a18bcec
Lucas Page-Caccia <lucas.page-caccia@mail.mcgill.ca>
2024-07-30 12:43:55 -0700

    cleanup wrapper

--- projects/modular_llm/src/utils/packed_attention_monkey_patch.py
+++ projects/modular_llm/src/utils/packed_attention_monkey_patch.py
@@ -2,159 +2,140 @@ import inspect
 import os
 from typing import Optional, Tuple
 
+import flash_attn
 import torch
 import torch.nn.functional as F
-from transformers.modeling_flash_attention_utils import (
-    _upad_input,
-    flash_attn_varlen_func,
-    is_flash_attn_2_available,
-    is_flash_attn_greater_or_equal,
-    pad_input,
-)
 
 from mttl.logging import warn_once
 from mttl.models.expert_context import InfoContainer
 
-if is_flash_attn_2_available():
-    from flash_attn import flash_attn_func, flash_attn_varlen_func
-    from flash_attn.bert_padding import index_first_axis, pad_input, unpad_input  # noqa
-
-    _flash_supports_window_size = "window_size" in list(
-        inspect.signature(flash_attn_func).parameters
-    )
+""" Pytorch SDPA Patching """
 
 
-def get_unpad_data(
-    attention_mask: torch.Tensor,
-) -> Tuple[torch.Tensor, torch.Tensor, int]:
-    """
-    Retrieves indexing data required to repad unpadded (ragged) tensors.
+def scaled_dot_product_attention(
+    query, key, value, attn_mask=None, dropout_p=0.0, is_causal=False, scale=None
+) -> torch.Tensor:
+    from mttl.models.expert_context import InfoContainer
 
-    Arguments:
-        attention_mask (`torch.Tensor`):
-            Boolean or int tensor of shape (batch_size, sequence_length), 1 means valid and 0 means not valid.
-
-    Return:
-        indices (`torch.Tensor):
-            The indices of non-masked tokens from the flattened input sequence.
-        cu_seqlens (`torch.Tensor`):
-            The cumulative sequence lengths, used to index into ragged (unpadded) tensors. `cu_seqlens` shape is (batch_size + 1,).
-        max_seqlen_in_batch (`int`):
-            Maximum sequence length in batch.
-    """
-
-    # 1) We try to fetch `packed_seq_lens` from the context. If it's there,
-    # then sequences are already packed.
     context = InfoContainer.get()
-    try:
-        cu_seqlens = context.routing_infos.packed_seq_lens
-        indices = torch.nonzero(attention_mask.flatten(), as_tuple=False).flatten()
-        max_seqlen_in_batch = context.routing_infos.seq_lens.max().item()
-        warn_once("\n\n\n\nUsing packed sequences with Flash Attention\n\n\n\n")
-    except AttributeError:
-        warn_once("\n\n\n\nNot packed sequences with Flash Attention\n\n\n\n")
-        seqlens_in_batch = attention_mask.sum(dim=-1, dtype=torch.int32)
-        indices = torch.nonzero(attention_mask.flatten(), as_tuple=False).flatten()
-        max_seqlen_in_batch = seqlens_in_batch.max().item()
-        cu_seqlens = F.pad(
-            torch.cumsum(seqlens_in_batch, dim=0, dtype=torch.int32), (1, 0)
-        )
 
-    return (
-        indices,
-        cu_seqlens,
-        max_seqlen_in_batch,
+    if attn_mask is not None:
+        bool_mask = attn_mask == 0  # True if we use for attention
+
+    if context._routing_infos.packed_seq_lens is not None:
+        # update the attention mask to account for packed sequences
+        # seq_lens = context._routing_infos.seq_lens
+        # mask = context._routing_infos.attention_mask
+        # bool_mask = context._routing_infos.packed_attn_mask# .to(attn_mask.dtype)
+        attn_mask = context._routing_infos.packed_attn_mask
+        is_causal = False
+
+    return torch.nn.functional._default_scaled_dot_product_attention(
+        query,
+        key,
+        value,
+        attn_mask=attn_mask,
+        dropout_p=dropout_p,
+        is_causal=is_causal,
+        scale=scale,
     )
 
 
-def flash_attention_forward(
-    query_states: torch.Tensor,
-    key_states: torch.Tensor,
-    value_states: torch.Tensor,
-    attention_mask: torch.Tensor,
-    query_length: int,
-    is_causal: bool,
-    dropout: float = 0.0,
-    softmax_scale: Optional[float] = None,
-    sliding_window: Optional[int] = None,
-    use_top_left_mask: bool = False,
-    softcap: Optional[float] = None,
-    deterministic: bool = os.environ.get("FLASH_ATTENTION_DETERMINISTIC", "0") == "1",
+""" Flash Attention Patching """
+
+
+def flash_attn_varlen_func_wrapper(
+    query_states,
+    key_states,
+    value_states,
+    cu_seqlens_q,
+    cu_seqlens_k,
+    max_seqlen_q,
+    max_seqlen_k,
+    dropout_p,
+    softmax_scale,
+    causal,
+    **flash_kwargs,
 ):
-    """
-    Calls the forward method of Flash Attention - if the input hidden states contain at least one padding token
-    first unpad the input, then computes the attention scores and pad the final attention scores.
+    if query_states.shape != key_states.shape:
+        raise ValueError("q and k must have the same shape")
 
-    Args:
-        query_states (`torch.Tensor`):
-            Input query states to be passed to Flash Attention API
-        key_states (`torch.Tensor`):
-            Input key states to be passed to Flash Attention API
-        value_states (`torch.Tensor`):
-            Input value states to be passed to Flash Attention API
-        attention_mask (`torch.Tensor`):
-            The padding mask - corresponds to a tensor of size `(batch_size, seq_len)` where 0 stands for the
-            position of padding tokens and 1 for the position of non-padding tokens.
-        dropout (`float`):
-            Attention dropout
-        softmax_scale (`float`, *optional*):
-            The scaling of QK^T before applying softmax. Default to 1 / sqrt(head_dim)
-        use_top_left_mask (`bool`, defaults to `False`):
-            flash_attn<2.1 generates top-left aligned causal mask, while what is needed here is bottom-right alignement, that was made default for flash_attn>=2.1. This attribute is used to handle this difference.
-        softcap (`float`, *optional*):
-            Softcap for the attention logits, used e.g. in gemma2.
-        deterministic (`bool`, *optional*):
-            Determines if the deterministic option introduced in flash_attn>=2.4.1 is enabled.
-    """
-    if not use_top_left_mask:
-        causal = is_causal
-    else:
-        # TODO: Remove the `query_length != 1` check once Flash Attention for RoCm is bumped to 2.1. For details, please see the comment in transformers.models.llama.modeling_llama.LlamaFlashAttention2.__init__.
-        causal = is_causal and query_length != 1
-
-    # Assuming 4D tensors, key_states.shape[1] is the key/value sequence length (source length).
-    use_sliding_windows = (
-        _flash_supports_window_size
-        and sliding_window is not None
-        and key_states.shape[1] > sliding_window
-    )
-    flash_kwargs = (
-        {"window_size": (sliding_window, sliding_window)} if use_sliding_windows else {}
-    )
-
-    if is_flash_attn_greater_or_equal("2.4.1"):
-        flash_kwargs["deterministic"] = deterministic
-
-    if softcap is not None:
-        flash_kwargs["softcap"] = softcap
-
-    # for some reason models like phi-2 remove the attention mask when there is no padding. This is a workaround to avoid that.
     context = InfoContainer.get()
-    attention_mask = context._routing_infos.attention_mask
+    if context.routing_infos.packed_seq_lens is not None:
+        warn_once("\n\n\n\nmade it here\n\n\n\n")
+        cu_seqlens_q = context.routing_infos.packed_seq_lens
+        cu_seqlens_k = context.routing_infos.packed_seq_lens
+        max_seqlen_q = context.routing_infos.seq_lens.max().item()
+        max_seqlen_k = context.routing_infos.seq_lens.max().item()
 
-    # Contains at least one padding token in the sequence
-    batch_size = query_states.shape[0]
-    query_states, key_states, value_states, indices_q, cu_seq_lens, max_seq_lens = (
-        _upad_input(
-            query_states, key_states, value_states, attention_mask, query_length
-        )
-    )
-    cu_seqlens_q, cu_seqlens_k = cu_seq_lens
-    max_seqlen_in_batch_q, max_seqlen_in_batch_k = max_seq_lens
-
-    attn_output_unpad = flash_attn_varlen_func(
+    return flash_attn._default_flash_attn_varlen_func(
         query_states,
         key_states,
         value_states,
-        cu_seqlens_q=cu_seqlens_q,
-        cu_seqlens_k=cu_seqlens_k,
-        max_seqlen_q=max_seqlen_in_batch_q,
-        max_seqlen_k=max_seqlen_in_batch_k,
-        dropout_p=dropout,
-        softmax_scale=softmax_scale,
-        causal=causal,
+        cu_seqlens_q,
+        cu_seqlens_k,
+        max_seqlen_q,
+        max_seqlen_k,
+        dropout_p,
+        softmax_scale,
+        causal,
         **flash_kwargs,
     )
-    attn_output = pad_input(attn_output_unpad, indices_q, batch_size, query_length)
 
-    return attn_output
+
+def flash_attn_func_wrapper(
+    q,
+    k,
+    v,
+    dropout_p=0.0,
+    softmax_scale=None,
+    causal=False,
+    window_size=(-1, -1),  # -1 means infinite context window
+    softcap=0.0,  # 0.0 means deactivated
+    alibi_slopes=None,
+    deterministic=False,
+    return_attn_probs=False,
+):
+    if q.shape != k.shape:
+        raise ValueError("q and k must have the same shape")
+
+    # assert there are no padding tokens if we get here
+    context = InfoContainer.get()
+    assert (context.routing_infos.attention_mask == 1).all()  # no padding tokens
+
+    if context.routing_infos.packed_seq_lens is not None:
+        cu_seqlens_q = cu_seqlens_k = context.routing_infos.packed_seq_lens
+        max_seqlen_q = max_seqlen_k = context.routing_infos.seq_lens.max().item()
+        q, k, v = q.flatten(0, 1), k.flatten(0, 1), v.flatten(0, 1)
+
+        return flash_attn_varlen_func_wrapper(
+            q,
+            k,
+            v,
+            cu_seqlens_q,
+            cu_seqlens_k,
+            max_seqlen_q,
+            max_seqlen_k,
+            dropout_p,
+            softmax_scale,
+            causal,
+            window_size=window_size,
+            softcap=softcap,
+            alibi_slopes=alibi_slopes,
+            deterministic=deterministic,
+            return_attn_probs=return_attn_probs,
+        )
+    else:
+        return flash_attn._default_flash_attn_func(
+            q,
+            k,
+            v,
+            dropout_p=dropout_p,
+            softmax_scale=softmax_scale,
+            causal=causal,
+            window_size=window_size,
+            softcap=softcap,
+            alibi_slopes=alibi_slopes,
+            deterministic=deterministic,
+            return_attn_probs=return_attn_probs,
+        )
