778ca090c2123b3f0dcbe413626572efb5e17e27
Alessandro Sordoni <alsordon@microsoft.com>
2024-08-03 15:24:04 -0700

    chat datamodule filter task name

--- mttl/datamodule/chat_data_module.py
+++ mttl/datamodule/chat_data_module.py
@@ -1,29 +1,48 @@
+import os
+from functools import partial
+
+import numpy
+
 from mttl.datamodule.base import DatasetConfig, DefaultDataModule
+from mttl.datamodule.mt_seq_to_seq_module import (
+    FlatMultiTaskConfig,
+    apply_source_template,
+)
+from mttl.datamodule.utils import maybe_filter_hf_dataset_by_task
+from mttl.logging import logger
 from mttl.models.library.expert_library import DatasetLibrary
 
 
-class ChatDataConfig(DatasetConfig):
-    chat_template: str = None  # TODO: load and apply custom chat template
+class ChatDataConfig(FlatMultiTaskConfig):
     seed: str = 42
 
 
 class ChatDataModule(DefaultDataModule):
     def setup_dataset(self):
-        dataset = DatasetLibrary.pull_dataset(self.config.dataset, split="train")
+        n_proc = int(os.environ.get("MTTL_NUM_PROC_DATASETS", 16))
+        dataset = DatasetLibrary.pull_dataset(self.config.dataset)
 
-        num_examples = len(dataset)
-        num_train = int(0.8 * num_examples)
+        (
+            self._task_names,
+            self._task_to_id,
+            train_dataset,
+            _,
+            _,
+        ) = maybe_filter_hf_dataset_by_task(
+            dataset,
+            "cluster_id",
+            self.config.finetune_task_name,
+            n_proc=n_proc,
+        )
+
+        num_examples = len(train_dataset)
+        num_train = int(0.9 * num_examples)
         num_dev = int(0.1 * num_examples)
 
-        dataset = dataset.shuffle(seed=self.config.seed)
+        train_dataset = train_dataset.shuffle(seed=self.config.seed)
+        train_dataset = train_dataset.rename_column("task_name", "original_task_name")
+        train_dataset = train_dataset.rename_column("cluster_id", "task_name")
 
-        dataset = dataset.rename_column("task_name", "origin_task_name")
-        dataset = dataset.rename_column("cluster_id", "task_name")
-        self._task_names = sorted(list(set(dataset["task_name"])))
-        self._task_to_id = {
-            task_name: i for i, task_name in enumerate(self._task_names)
-        }
-
-        self.train_dataset = dataset.select(range(num_train))
-        self.dev_dataset = dataset.select(range(num_train, num_train + num_dev))
-        self.test_dataset = dataset.select(range(num_train + num_dev, num_examples))
+        self.train_dataset = train_dataset.select(range(num_train))
+        self.dev_dataset = train_dataset.select(range(num_train, num_train + num_dev))
+        self.test_dataset = self.dev_dataset
