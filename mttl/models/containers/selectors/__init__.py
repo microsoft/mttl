1ec403ac0204cd3a2ada6cde6eec4ac515051823
Alessandro Sordoni <alsordon@microsoft.com>
2024-08-08 06:16:56 -0700

    remove import *

--- mttl/models/containers/selectors/__init__.py
+++
@@ -1,6 +0,0 @@
-from mttl.models.containers.selectors.arrow_selector import *
-from mttl.models.containers.selectors.average_activation_selector import *
-from mttl.models.containers.selectors.base import *
-from mttl.models.containers.selectors.moe_selector import *
-from mttl.models.containers.selectors.phatgoose_selector import *
-from mttl.models.containers.selectors.poly_selector import *
