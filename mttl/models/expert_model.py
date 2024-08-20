c5f450ad3c8e5cf720ce37f8f299aea684f434fa
Alessandro Sordoni <alsordon@microsoft.com>
2024-08-19 19:45:39 -0700

    separate utils and pl utils

--- mttl/models/expert_model.py
+++ mttl/models/expert_model.py
@@ -29,7 +29,7 @@ from mttl.models.modifiers import modify_transformer
 from mttl.models.modifiers.base import Modifier, ModifierConfig
 from mttl.models.modifiers.lora import SkilledLoRAConfig
 from mttl.models.modifiers.modify_model import get_modifier_name
-from mttl.models.utils import EfficientCheckpointModule, prepare_model_for_kbit_training
+from mttl.models.pl_utils import EfficientCheckpointModule
 
 torch.set_float32_matmul_precision("high")
 
