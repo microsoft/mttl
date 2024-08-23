12ec534a414b1485f644d11e42eff4d5aab5c1aa
Matheus Pereira <matpereira@microsoft.com>
2024-08-23 12:35:36 -0700

    nit test_peer

--- tests/test_peer.py
+++ tests/test_peer.py
@@ -1,32 +1,8 @@
-import functools
-import os
-
 import numpy as np
 import pytest
-import torch
-from pytorch_lightning import seed_everything
 
-from mttl.config import MoEExpertConfig, MultiExpertConfig
-from mttl.models.containers.lora_containers import (
-    CoalescedLoRAExpertContainer,
-    LoRAExpertContainer,
-)
-from mttl.models.containers.selectors.base import LoadableLibraryMixin
-from mttl.models.containers.selectors.moe_selector import MOERKHSSelector
-from mttl.models.containers.selectors.per_token_selector import PerTokenSelector
-from mttl.models.containers.selectors.poly_selector import (
-    PolySelector,
-    PolySelectorConfig,
-    PolySelectorDirect,
-)
-from mttl.models.containers.selectors.selector_output import (
-    BatchSequenceExpertsAndWeightsSelectorOutput,
-    SelectorOutput,
-)
-from mttl.models.expert_model import MoEModel, MultiExpertModel
-from mttl.models.library.expert import Expert
-from mttl.models.modifiers.base import ModifierConfig
-from mttl.models.modifiers.lora import LoRA
+from mttl.config import MoEExpertConfig
+from mttl.models.expert_model import MoEModel
 
 
 def test_peer_moe(tmp_peer_moe_config, dummy_batch):
