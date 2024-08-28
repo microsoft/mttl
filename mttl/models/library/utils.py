52c05a0205823b57e4efa2f56b6d99988eaa6d32
Matheus Pereira <matpereira@microsoft.com>
2024-08-28 12:45:42 -0700

    move get_svd_embedding to retrievers.py

--- mttl/models/library/utils.py
+++ mttl/models/library/utils.py
@@ -91,13 +91,3 @@ def train_module(args: ExpertConfig, module: ExpertModule, dm):
         checkpoint_callback.best_model_path or checkpoint_callback.last_model_path
     )
     return checkpoint
-
-
-def get_svd_embedding(lib, expert_name: str):
-    try:
-        embeddings = lib.get_auxiliary_data(
-            data_type="embeddings", expert_name=expert_name
-        )
-    except ValueError:
-        return None
-    return embeddings["svd"]["embeddings"]
