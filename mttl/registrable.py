d324e7aaf6cfad0d975792d62712b7f4e2d2df0d
Alessandro Sordoni <alsordon@microsoft.com>
2024-09-24 18:03:12 -0700

    raise when name is not registered + fix import

--- mttl/registrable.py
+++ mttl/registrable.py
@@ -13,36 +13,57 @@ class Registrable:
 
         def add_to_registry(subclass):
             if name in registry:
-                raise ValueError(f"Cannot register {name} multiple times.")
+                if subclass != registry[name][0]:
+                    raise ValueError(
+                        f"Cannot register '{name}' multiple times for class '{cls.__name__}'."
+                    )
+                else:
+                    return subclass
 
             logger.info(
                 "Registering %s: adding %s as %s", cls.__name__, subclass.__name__, name
             )
             registry[name] = (subclass, config_cls)
+
+            # add the registered name to the subclass as a field
+            subclass.registered_name = name
             return subclass
 
         return add_to_registry
 
     @classmethod
     def get_config_class_by_name(cls, name: str) -> Type:
-        subclass, config_cls = Registrable._registry[cls].get(name)
+        result = Registrable._registry[cls].get(name)
+        if result is None:
+            raise ValueError(f"'{name}' is not registered for class '{cls.__name__}'.")
+        _, config_cls = result
+        if config_cls is None:
+            raise ValueError(
+                f"No config class associated with name '{name}' for class '{cls.__name__}'."
+            )
         return config_cls
 
     @classmethod
     def get_class_by_name(cls, name: str) -> Type:
-        subclass, config_cls = Registrable._registry[cls].get(name)
+        result = Registrable._registry[cls].get(name)
+        if result is None:
+            raise ValueError(f"'{name}' is not registered for class '{cls.__name__}'.")
+        subclass, _ = result
         return subclass
 
     @classmethod
     def get_name_by_config_class(cls, config_class: Type) -> str:
-        for key, value in Registrable._registry[cls].items():
-            subclass, config_cls = value
+        for key, (subclass, config_cls) in Registrable._registry[cls].items():
             if config_cls == config_class:
                 return key
+        raise ValueError(
+            f"Config class '{config_class}' is not registered for class '{cls.__name__}'."
+        )
 
     @classmethod
     def get_class_by_config_class(cls, config_class: Type) -> Type:
-        return cls.get_class_by_name(cls.get_name_by_config_class(config_class))
+        name = cls.get_name_by_config_class(config_class)
+        return cls.get_class_by_name(name)
 
     @classmethod
     def registered_names(cls) -> List[str]:
@@ -51,7 +72,7 @@ class Registrable:
     @classmethod
     def registered_configs(cls) -> List[Type]:
         return [
-            config
-            for _, config in Registrable._registry[cls].values()
-            if config is not None
+            config_cls
+            for _, config_cls in Registrable._registry[cls].values()
+            if config_cls is not None
         ]
