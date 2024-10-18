3cbadf5f9f55f9b4b5db27014047fd180bc66f06
Lucas Caccia <lucas.page-caccia@mail.mcgill.ca>
2024-10-18 12:16:16 -0700

    mini nqa dataset

--- projects/kms/nqa_datamodule.py
+++ projects/kms/nqa_datamodule.py
@@ -1,3 +1,4 @@
+import json
 from dataclasses import dataclass
 
 from mttl.datamodule.base import DataModule, DatasetConfig
@@ -11,6 +12,7 @@ class NQADatasetConfig(DatasetConfig):
     prompt: str = "Answer the following question: "
     include_context: bool = False
     topk_context: int = 10
+    subsample_file: str = None
 
 
 @DataModule.register("narrativeqa", config_cls=NQADatasetConfig)
@@ -19,6 +21,26 @@ class NQADatamodule(DataModule):
         from mttl.models.library.dataset_library import DatasetLibrary
 
         dataset = DatasetLibrary.pull_dataset(self.config.dataset)
+
+        # Instead of always working with the large NQA dataset, we can subsample it
+        # This allows us to reuse our previous datasets, while ensure a consistent train / dev / test split
+        if self.config.subsample_file:
+            subsample_file = json.load(open(self.config.subsample_file, "r"))
+            doc_to_split = {
+                doc: split
+                for split in ["train", "dev", "test"]
+                for doc in subsample_file[split]
+            }
+            all_docs = set(
+                subsample_file["train"] + subsample_file["dev"] + subsample_file["test"]
+            )
+            dataset = dataset.filter(lambda x: x["document_id"] in all_docs)
+
+            def update_split(item):
+                item["split"] = doc_to_split[item["document_id"]]
+
+            # Update the Split column
+            dataset = dataset.map(update_split)
 
         (
             self._task_names,
@@ -120,3 +142,13 @@ class NQADatamodule(DataModule):
                 num_proc=16,
                 remove_columns=self.test_dataset.column_names,
             )
+
+
+@dataclass
+class MiniNQADatasetConfig(NQADatasetConfig):
+    subsample_file: str = "nqa_mini_split.json"
+
+
+@DataModule.register("mini_nqa", config_cls=MiniNQADatasetConfig)
+class MiniNQADatamodule(NQADatamodule):
+    pass
