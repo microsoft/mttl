504e23a69ae569b834f19166c47737f1b89b9627
Lucas Caccia <lucas.page-caccia@mail.mcgill.ca>
2024-11-25 10:02:09 -0800

    update

--- projects/kms/train_km_closedform.py
+++ projects/kms/train_km_closedform.py
@@ -129,7 +129,7 @@ def train_km(training_args: KMArguments):
         raise ValueError(f"Loss function {training_args.loss_function} not supported")
 
     datamodule = get_datamodule(training_args)
-    val_loss, rougeL = do_evaluation(datamodule, model, loss_function, evaluator)
+    # val_loss, rougeL = do_evaluation(datamodule, model, loss_function, evaluator)
 
     # For every LoRA layer, let's set up a hook to fetch both the input and outputs of a forward pass
     # This is useful for computing the loss function
@@ -139,17 +139,20 @@ def train_km(training_args: KMArguments):
             use_context = info_container["use_context"]
 
             if use_context:  # store the input
+                module.input += [input[0][labels != -100].detach().cpu()]
                 module.output += [output[labels != -100].detach().cpu()]
             else:
-                module.input += [input[0][nc_labels != -100].detach().cpu()]
+                module.nc_input += [input[0][nc_labels != -100].detach().cpu()]
+                module.nc_output += [output[nc_labels != -100].detach().cpu()]
 
-            assert abs(len(module.input) - len(module.output)) <= 1
+            assert abs(len(module.nc_input) - len(module.input)) <= 1
 
         hooks = []
-        breakpoint()
         for name, module in model.named_modules():
             if isinstance(module, Modifier):
                 logger.info(f"Setting up hook for {name}")
+                module.nc_input = []
+                module.nc_output = []
                 module.input = []
                 module.output = []
                 hook = module.register_forward_hook(partial(hook_fn, name))
@@ -200,9 +203,6 @@ def train_km(training_args: KMArguments):
                     attention_mask=nc_attention_mask,
                 )
 
-                # Compute DCD loss between inputs and outputs
-                # and check whether iterating over time helps
-
             # Now, we iterate over the layers, and solve closed form LoRA values
             for name, module in model.named_modules():
                 if isinstance(module, Modifier):
@@ -219,16 +219,31 @@ def train_km(training_args: KMArguments):
                     # A = torch.stack(As).mean(dim=0)
                     # B = torch.stack(Bs).mean(dim=0)
                     input = torch.cat(module.input, dim=0)
+                    nc_input = torch.cat(module.nc_input, dim=0)
                     output = torch.cat(module.output, dim=0)
+                    nc_output = torch.cat(module.nc_output, dim=0)
+
+                    print(f"diff input: {torch.norm(input - nc_input)}")
+                    print(f"diff output: {torch.norm(output - nc_output)}")
+
+                    Y = (output - nc_output).float().cuda()
+                    X = (nc_input).float().cuda()
+
                     A, B = closed_form_low_rank_solution(
-                        input.float()[::20],
-                        output.float()[::20],
+                        X[::20],
+                        Y[::20],
                         module.config.lora_rank,
                         lambda_reg=100,
                     )
 
+                    # breakpoint()
+                    # pred y
+                    pred_y = X @ A @ B
+                    print(f"pred_y: {torch.norm(pred_y - Y)}")
+
                     module.lora_a.data.copy_(A.data)
                     module.lora_b.data.copy_(B.data)
+                    # '''
 
             remove_hooks(hooks)
             val_loss, rougeL = do_evaluation(
