6bc59ed1872b0377eb333ba42b4d6f381adfcf9b
Lucas Caccia <lucas.page-caccia@mail.mcgill.ca>
2024-11-25 17:35:27 -0800

    ddp safe wandb callback

--- projects/kms/utils/callbacks.py
+++ projects/kms/utils/callbacks.py
@@ -1,5 +1,7 @@
-import wandb
 from transformers import TrainerCallback
+
+import wandb
+from mttl.utils import logger
 
 
 class LogMttlArgs(TrainerCallback):
@@ -8,5 +10,12 @@ class LogMttlArgs(TrainerCallback):
         self.additional_args = additional_args
 
     def on_train_begin(self, args, state, control, **kwargs):
+        # check if wandb was initialized
+        if wandb.run is None:
+            logger.warning(
+                "Wandb is not initialized. Skipping logging of additional args."
+            )
+            return
+
         # Log any additional arguments at the start of trainixg
         wandb.config.update(self.additional_args, allow_val_change=True)
