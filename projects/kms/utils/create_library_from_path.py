026d1a677605502f262a67bbf63f42d26c50c773
Lucas Caccia <lucas.page-caccia@mail.mcgill.ca>
2024-12-04 07:02:23 -0800

    support for libs trained with `train_km_simple`

--- projects/kms/utils/create_library_from_path.py
+++ projects/kms/utils/create_library_from_path.py
@@ -8,7 +8,7 @@ import torch
 from mttl.models.library.expert_library import ExpertLibrary, LocalExpertLibrary
 
 
-def find_directories_with_config(start_dir):
+def find_directories_with_config(start_dir, model_name="best_model"):
     matching_dirs = set()
 
     # this dir is a list of ids
@@ -18,10 +18,10 @@ def find_directories_with_config(start_dir):
         # loop through the second level of directories
         checkpoint_dirs = []
         for ckpt in glob.glob(os.path.join(id, "*")):
-            if "checkpoint" in ckpt or "best_model" in ckpt:
-                checkpoint_dirs.append(os.path.join(start_dir, id, "best_model/"))
+            if "checkpoint" in ckpt or model_name in ckpt:
+                checkpoint_dirs.append(os.path.join(start_dir, id, f"{model_name}"))
 
-        has_best_model = [ckpt for ckpt in checkpoint_dirs if "best_model" in ckpt]
+        has_best_model = [ckpt for ckpt in checkpoint_dirs if model_name in ckpt]
         if has_best_model:
             matching_dirs.add(has_best_model[0])
         else:
@@ -33,7 +33,8 @@ def find_directories_with_config(start_dir):
 @click.command()
 @click.option("--ckpt_path", type=str)
 @click.option("--library_path", type=str)
-def create(ckpt_path, library_path):
+@click.option("--model_name", type=str, default="best_model")
+def create(ckpt_path, library_path, model_name):
     library = ExpertLibrary.get_expert_library(library_path, create=True)
     expert_paths, missing = find_directories_with_config(ckpt_path)
     if missing:
@@ -41,10 +42,20 @@ def create(ckpt_path, library_path):
         print(missing)
 
     library.add_experts_from_ckpts(expert_paths, force=False, update=True)
-    training_args = json.dumps(
-        torch.load(os.path.join(expert_paths[0], "mttl_args.bin"), weights_only=False),
-        indent=4,
-    )
+
+    # Outputs from `train_km.py`
+    if os.path.exists(os.path.join(expert_paths[0], "mttl_args.bin")):
+        training_args = json.dumps(
+            torch.load(
+                os.path.join(expert_paths[0], "mttl_args.bin"), weights_only=False
+            ),
+            indent=4,
+        )
+    # Outputs form `train_km_simple.py`
+    else:
+        training_args = json.load(
+            open(os.path.join(expert_paths[0], "config.json"), "r")
+        )
     library.update_readme(
         extra_info=f"Training arguments:\n```json\n{training_args}\n```"
     )
