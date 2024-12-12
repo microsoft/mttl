8b0ad0bc6efadeda25d81a3efec4723070c20c57
zhansu <zhan.su@di.ku.dk>
2024-12-12 15:28:17 -0500

    fix gsm evaluation

--- mttl/evaluators/gsm_evaluator.py
+++ mttl/evaluators/gsm_evaluator.py
@@ -2,6 +2,7 @@ import os
 
 from tqdm.auto import tqdm
 from mttl.evaluators.base import GenerativeEvaluator, switch_to_eval_mode
+import re
 
 
 class GsmEvaluator(GenerativeEvaluator):
@@ -46,18 +47,28 @@ class GsmEvaluator(GenerativeEvaluator):
 
         for num_batch, batch in pbar:
             predictions = self.generate_for_batch(model, batch)
+            all_predictions.extend(predictions.sequences_texts)
+            all_targets.extend(batch["labels_texts"])
 
-            all_predictions.extend(predictions)
-            all_targets.extend(batch["target"])
-        breakpoint()
         metrics = self.compute_metrics(all_predictions, all_targets)
         return metrics
 
     def compute_metrics(self, predictions, targets):
         # compute the accuracy
         correct = 0
-        for pred, target in zip(predictions, targets):
-            if pred == target:
+
+        for pred, target in tqdm(zip(predictions, targets)):
+
+            sentence = pred
+            predictions = sentence.replace(",", "")
+            # code is from https://github.com/aksh555/LoRA-Soups/blob/main/utils.py#L224
+            pred = [s for s in re.findall(r"-?\d+\.?\d*", predictions)]
+            if not pred:
+                return float("inf")
+            pred_answer = float(pred[-1])
+            if pred_answer == float(target):
                 correct += 1
+            print(pred_answer, float(target))
+
         accuracy = correct / len(predictions)
         return accuracy
