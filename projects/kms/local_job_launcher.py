b06938f1e26de8c39ac5a4724f6430d8611cd4e2
Lucas Caccia <lucas.page-caccia@mail.mcgill.ca>
2024-12-30 12:12:57 -0800

    launcher id

--- projects/kms/local_job_launcher.py
+++ projects/kms/local_job_launcher.py
@@ -1,9 +1,11 @@
 import asyncio
 import json
 import os
+import random
 import signal
 import subprocess
 import sys
+import time
 from contextlib import asynccontextmanager
 from time import sleep
 
@@ -32,6 +34,35 @@ class JobQueue:
         self.n_retries = n_retries
         print(f"queue file : {self.queue_file}")
 
+        self.launcher_id = f"{random.randint(1000, 9999)}-{os.getpid()}"
+        self.heartbeat_file = os.path.join(
+            os.path.dirname(lock_file), f"launcher_{self.launcher_id}.txt"
+        )
+        with open(self.heartbeat_file, "w") as f:
+            f.write("heartbeat")
+
+    async def keep_heartbeat_alive(self):
+        while True:
+            with open(self.heartbeat_file, "a") as f:
+                f.write(f"{time.time()}\n")
+            await asyncio.sleep(30)
+
+    async def mark_launcher_as_crashed(self, dead_id):
+        async with AsyncFileLock(self.lock_file):
+            if os.path.exists(self.queue_file):
+                print(f"marking launcher {dead_id} as crashed")
+                with open(self.queue_file, "r") as f:
+                    tasks = json.load(f)
+                for task in tasks:
+                    if (
+                        task.get("launcher_id") == dead_id
+                        and task["status"] == "running"
+                    ):
+                        task["status"] = "crashed"
+                        task["launcher_id"] = None
+                with open(self.queue_file, "w") as f:
+                    json.dump(tasks, f, indent=2)
+
     async def initialize_queue_file(self):
         async with AsyncFileLock(self.lock_file):
             if not os.path.exists(self.queue_file):
@@ -51,9 +82,14 @@ class JobQueue:
                     # If data is already a single list, just use it directly.
                     doc_ids = data
 
-                # Convert to list of {"doc_id": ..., "status": "queued", "retries": 0}
+                # Convert to list of {"doc_id": ..., "status": "queued", "retries": 0, "launcher_id": None}
                 tasks = [
-                    {"doc_id": doc_id, "status": "queued", "retries": 0}
+                    {
+                        "doc_id": doc_id,
+                        "status": "queued",
+                        "retries": 0,
+                        "launcher_id": None,
+                    }
                     for doc_id in doc_ids
                 ]
 
@@ -72,6 +108,7 @@ class JobQueue:
                 if task["status"] == "queued":
                     # Mark as running
                     task["status"] = "running"
+                    task["launcher_id"] = self.launcher_id
                     doc_id = task["doc_id"]
                     break
 
@@ -82,6 +119,7 @@ class JobQueue:
                         if task["retries"] < self.n_retries:
                             # Mark as running
                             task["status"] = "running"
+                            task["launcher_id"] = self.launcher_id
                             task["retries"] += 1
                             doc_id = task["doc_id"]
                             break
@@ -109,6 +147,8 @@ class JobQueue:
                 if task["doc_id"] in doc_id:
                     # For other statuses, set the status directly
                     task["status"] = new_status
+                    if new_status != "running":
+                        task["launcher_id"] = None
 
                     if len(doc_id) == 1:
                         break
@@ -123,8 +163,14 @@ class JobQueue:
         queued = len([task for task in tasks if task["status"] == "queued"])
         failed = len([task for task in tasks if task["status"] == "failed"])
         crashed = len([task for task in tasks if task["status"] == "crashed"])
+        basedir = os.path.dirname(self.lock_file)
+        active_launchers_count = sum(
+            1
+            for f in os.listdir(basedir)
+            if f.startswith("launcher_") and f.endswith(".txt")
+        )
         print(
-            f"***\t\tProgress: {done}/{total} done, {running} running, {queued} queued, {failed} failed, {crashed} crashed\t\t***"
+            f"***\t\t [{self.launcher_id}, {active_launchers_count}] Progress: {done}/{total} done, {running} running, {queued} queued, {failed} failed, {crashed} crashed\t\t***"
         )
 
     async def read_queue_file(self):
@@ -159,7 +205,7 @@ async def run_job(gpu_id, doc_id, config_file, output_dir, train_file, config_id
 
     async def stream_output(stream, prefix, job_name):
         job_name = str(job_name)[:10]
-        job_name = job_name.ljust(10)
+        job_name = job_name.ljust(9)
         try:
             async for line in stream:
                 print(f"{prefix} <{job_name}>: {line.decode().strip()}")
@@ -225,6 +271,32 @@ def parse_arguments():
     )
 
 
+async def cleanup_dead_launchers(lock_file, job_queue):
+    basedir = os.path.dirname(lock_file)
+    while True:
+        for fn in os.listdir(basedir):
+            if fn.startswith("launcher_") and fn.endswith(".txt"):
+                path = os.path.join(basedir, fn)
+                st = os.stat(path)
+                last_touched = st.st_mtime
+                current_time = time.time()
+                if (current_time - last_touched) > 600:
+                    dead_id = fn[len("launcher_") : -4]
+                    print(
+                        f"Cleaning up heartbeat file {fn} for launcher {dead_id}. Last touched {last_touched} & current time {current_time}"
+                    )
+                    if dead_id == job_queue.launcher_id:
+                        print(f"Current launcher {dead_id} is dead !!??")
+                        continue
+
+                    await job_queue.mark_launcher_as_crashed(dead_id)
+                    new_name = f"trace_{dead_id}__{job_queue.launcher_id}.txt"
+                    os.rename(path, os.path.join(basedir, new_name))
+                    print(f"Renamed {fn} to {new_name}")
+
+        await asyncio.sleep(60)
+
+
 async def main():
     num_gpus, jobs_per_gpu, job_file, config_id, train_file, n_retries = (
         parse_arguments()
@@ -258,6 +330,8 @@ async def main():
     running_tasks = []
 
     monitor_task = asyncio.create_task(job_queue.print_progress())
+    hearbeat_task = asyncio.create_task(job_queue.keep_heartbeat_alive())
+    cleanup_task = asyncio.create_task(cleanup_dead_launchers(lock_file, job_queue))
 
     while True:
         # If we received an exit signal (SIGTERM or SIGINT), shut down gracefully
@@ -272,7 +346,10 @@ async def main():
             # Cancel all running tasks and mark as crashed
             for t, gpu_id, doc_id in running_tasks:
                 t.cancel()
+
             monitor_task.cancel()
+            hearbeat_task.cancel()
+            cleanup_task.cancel()
 
             # print one more time job status
             tasks = await job_queue.read_queue_file()
@@ -285,6 +362,8 @@ async def main():
                 )
                 running_tasks.clear()
 
+            # delete the heartbeat file
+            # os.remove(job_queue.heartbeat_file)
             logger.warning("All running tasks cancelled. Exiting.")
             break
 
@@ -294,7 +373,6 @@ async def main():
                 doc_id = await job_queue.get_next_job()
                 if not doc_id:
                     break
-                task = None
                 task = asyncio.create_task(
                     run_job(
                         gpu_id, doc_id, config_file, output_dir, train_file, config_id
