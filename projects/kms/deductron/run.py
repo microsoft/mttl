bef41306341e2a44240af91f4d441a18187bde1b
Alessandro Sordoni <alsordon@microsoft.com>
2025-01-07 10:50:29 -0800

    remove deepspeed

--- projects/kms/deductron/run.py
+++ projects/kms/deductron/run.py
@@ -271,7 +271,6 @@ if __name__ == "__main__":
         "--fast", action="store_true", help="Use fast mode (no eval on epoch 0)"
     )
     parser.add_argument("-d", type=str, help="Run description", default=None)
-    parser = deepspeed.add_config_arguments(parser)
 
     # parse known args first
     partial_args, unknown_args = parser.parse_known_args()
