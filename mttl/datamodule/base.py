231ae9868280093d21a7a80dd3038dedee787b5d
zhansu <zhan.su@di.ku.dk>
2025-01-15 03:32:44 -0500

    fix training bug for moe with library

--- mttl/datamodule/base.py
+++ mttl/datamodule/base.py
@@ -946,6 +946,10 @@ def get_datamodule(args, for_generation=False, dataset_override=None):
     )
     from mttl.datamodule.mathqa_data_module import MathQADataConfig, MathQADataModule
     from mttl.datamodule.gsm_data_module import GsmDataConfig, GsmDataModule
+    from mttl.datamodule.abstention_data_module import (
+        AbstentionDataConfig,
+        AbstentionDataModule,
+    )
     from mttl.datamodule.base import DatasetConfig
     from mttl.datamodule.alpaca_data_module import (
         AlpacaCodeDataModule,
@@ -1054,6 +1058,9 @@ def get_datamodule(args, for_generation=False, dataset_override=None):
         assert not for_generation
         config = dataset_to_klass_map[dataset][0]
         dm = dataset_to_klass_map[dataset][1](config)
+    elif "coconot" in dataset:
+        config = AbstentionDataConfig(**common_kwargs)
+        dm = AbstentionDataModule(config, for_generation=for_generation)
     elif "flan" in dataset:
         config = FlanConfig(
             **common_kwargs,
