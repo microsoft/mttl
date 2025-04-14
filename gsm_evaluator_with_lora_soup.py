77518b383d2ad7b8be1c3f62e177dfd3f516cca9
zhansu <zhan.su@di.ku.dk>
2025-04-13 22:37:12 -0400

    add arrow routing

--- gsm_evaluator_with_lora_soup.py
+++ gsm_evaluator_with_lora_soup.py
@@ -20,6 +20,52 @@ from mttl.models.library.library_transforms import (
     WeightedLinearMergeConfig,
 )
 
+def fetch_prototypes(args: EvaluationConfig, library: ExpertLibrary) -> str:
+    """Returns the unique hash storing the saved prototypes."""
+    if args.merge_or_route == "phatgoose":
+        from mttl.models.containers.selectors.phatgoose_selector import (
+            compute_phatgoose_embeddings,
+        )
+
+        return compute_phatgoose_embeddings(
+            library,
+            selector_data_id=args.selector_data_id,
+            n_steps_pg=args.n_steps_pg,
+            learning_rate_pg=args.learning_rate_pg,
+            recompute_prototypes=args.recompute_prototypes,
+            default_args=args,
+        )
+    elif args.merge_or_route == "arrow":
+        from mttl.models.containers.selectors.arrow_selector import (
+            compute_arrow_embeddings,
+        )
+
+        return compute_arrow_embeddings(
+            library,
+            selector_data_id=args.selector_data_id,
+            ab_only=args.ab_only,
+            tie_params=args.tie_params,
+            tie_op=args.tie_op,
+            recompute_prototypes=args.recompute_prototypes,
+        )
+    elif args.merge_or_route == "hidden":
+        from mttl.models.containers.selectors.average_activation_selector import (
+            compute_hidden_states,
+        )
+
+        return compute_hidden_states(
+            library,
+            selector_data_id=args.selector_data_id,
+            use_base_model_only=args.use_base_model_only,
+            max_samples_per_task=args.max_samples_per_task,
+            recompute_prototypes=args.recompute_prototypes,
+            track=args.track,
+            pool=args.pool,
+            default_args=args,
+        )
+    else:
+        raise ValueError(f"Unknown merge_or_route {args.merge_or_route}")
+
 config = GsmDataConfig(
     model=args.model,
     model_family=args.model_family,
@@ -43,6 +89,29 @@ else:
     if args.merge_or_route == "uniform":
         module.add_experts_from_library(args.library_id)
         module.model.set_selector("lora", UniformSelectorConfig(lora_merge_after=args.lora_merge_after))
+    elif args.merge_or_route in ["phatgoose", "arrow", "avg_act"]:
+        module.add_experts_from_library(args.library_id)
+        """Routing Approaches"""
+        from mttl.models.containers.selectors import (
+            ArrowSelectorConfig,
+            AverageActivationSelectorConfig,
+            PhatgooseSelectorConfig,
+        )
+
+        # compute prototypes if not provided
+        if args.merge_or_route == "phatgoose":
+            selector_config = PhatgooseSelectorConfig.from_training_config(args)
+        elif args.merge_or_route == "arrow":
+            selector_config = ArrowSelectorConfig.from_training_config(args)
+        elif args.merge_or_route == "avg_act":
+            selector_config = AverageActivationSelectorConfig.from_training_config(args)
+
+        # if a specific prototype hash is *not* specified in the config, compute it and store them in the library
+        # otherwise, the selector data id will be used to load the prototypes automatically
+        if not selector_config.selector_data_id:
+            selector_config.selector_data_id = fetch_prototypes(args, library)
+        module.model.set_selector("lora", selector_config)
+
     elif args.expert_selection is not None:
         expert = library.get_expert(args.expert_selection)
         module.add_expert_instance(expert, is_default=True)
