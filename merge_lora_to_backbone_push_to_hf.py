0ce6c97adc8027f51c03b147ede60bf7fffc87dc
zhansu <zhan.su@di.ku.dk>
2025-05-10 18:37:37 -0400

    update merge

--- merge_lora_to_backbone_push_to_hf.py
+++ merge_lora_to_backbone_push_to_hf.py
@@ -5,6 +5,11 @@ from mttl.models.containers.selectors.base import UniformSelectorConfig
 from mttl.models.library.expert_library import ExpertLibrary
 import torch
 from mttl.logging import setup_logging
+from mttl.models.library.library_transforms import (
+    WeightedLinearMerge,
+    WeightedLinearMergeConfig,
+)
+
 setup_logging()
 args = EvaluationConfig.parse()
 if args.library_id is None:
@@ -15,22 +20,26 @@ else:
     module = MultiExpertModule(**vars(args))
 
     if args.merge_or_route == "uniform":
-        module.add_experts_from_library(args.library_id)
-        module.model.set_selector("lora", UniformSelectorConfig(lora_merge_after=args.lora_merge_after))
+        expert = WeightedLinearMerge(WeightedLinearMergeConfig()).transform(library)
+        module.add_expert_instance(expert, is_default=True)
 if args.checkpoint is not None:
     checkpoint = torch.load(args.checkpoint, weights_only=False)["state_dict"]
     module.load_state_dict(checkpoint)
-
+    del checkpoint
 
 if args.save_merged_model:
-    module.model.merge_and_save_base_model(args.output_dir)
+    if isinstance(module, MultiExpertModule):
+        module.model.merge_and_save_base_model(
+            args.output_dir, expert_name="weighted_expert"
+        )
+    else:
+        # if the module is ExpertModule, we can save it directly.
+        module.model.merge_and_save_base_model(args.output_dir)
     # clear the gpu memory
     torch.cuda.empty_cache()
     del module
-    del checkpoint
-    tokenizer = AutoTokenizer.from_pretrained(args.output_dir)
-    model = AutoModelForCausalLM.from_pretrained(args.output_dir)
-    tokenizer.push_to_hub(args.save_merged_model)
-    model.push_to_hub(args.save_merged_model)
 
-
+    # tokenizer = AutoTokenizer.from_pretrained(args.output_dir)
+    # model = AutoModelForCausalLM.from_pretrained(args.output_dir)
+    # tokenizer.push_to_hub(args.save_merged_model)
+    # model.push_to_hub(args.save_merged_model)
