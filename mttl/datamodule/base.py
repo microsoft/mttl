445f239d1641b72341866697e110e24e2138b562
zhansu <zhan.su@di.ku.dk>
2025-08-08 15:53:46 -0400

    rm gsm

--- mttl/datamodule/base.py
+++ mttl/datamodule/base.py
@@ -957,7 +957,6 @@ def get_datamodule(args, for_generation=False, dataset_override=None):
         HellaswagMultiChoiceDataModule,
     )
     from mttl.datamodule.mathqa_data_module import MathQADataConfig, MathQADataModule
-    from mttl.datamodule.gsm_data_module import GsmDataConfig, GsmDataModule
     from mttl.datamodule.abstention_data_module import (
         AbstentionDataConfig,
         AbstentionDataModule,
